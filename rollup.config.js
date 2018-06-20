0ba8a34c6fd8b578188d83839635bba437fef0bd
Julien Fraichot <jfraichot@learningmachine.com>
2018-06-20 13:46:46 +0200

    chore(ESM): configure correctly to transpile

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,11 @@
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import replace from 'rollup-plugin-re';
+
 export default {
   input: 'lib/index.js',
+  name: 'Verifier',
   output: [
     {
       file: 'verifier.js',
@@ -9,5 +15,50 @@ export default {
       file: 'verifier-es.js',
       format: 'es'
     }
+  ],
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: false
+    }),
+    // https://github.com/rollup/rollup-plugin-commonjs/issues/166
+    // fix issue with jsonld
+    replace({
+      patterns: [
+        {
+          // regexp match with resolved path
+          match: /jsonld.js/,
+          // string or regexp
+          test: 'global = window;',
+          // string or function to replaced with
+          replace: '',
+        },
+        {
+          // regexp match with resolved path
+          match: /jsonld.js/,
+          // string or regexp
+          test: 'global = self;',
+          // string or function to replaced with
+          replace: '',
+        },
+        {
+          // regexp match with resolved path
+          match: /jsonld.js/,
+          // string or regexp
+          test: 'global = $;',
+          // string or function to replaced with
+          replace: '',
+        }
+      ]
+
+    }),
+    commonjs({
+      namedExports: {
+        'debug': ['debug'],
+        'bitcoinjs-lib': ['bitcoin'],
+        'jsonld': ['jsonld']
+      }
+    }),
+    json(),
   ]
 };

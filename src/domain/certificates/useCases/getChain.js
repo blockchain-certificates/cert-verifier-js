a0fad9b9f954c0f1a1f720ac1aaeceddbd8da47b
Matthieu COLLÉ <druidev+github@gmail.com>
2018-07-20 10:38:02 -0500

    feat(chain): improved getChain

--- src/domain/certificates/useCases/getChain.js
+++ src/domain/certificates/useCases/getChain.js
@@ -10,18 +10,23 @@ import addresses from '../../addresses';
  * @param bitcoinAddress
  * @returns {*}
  */
-export default function getChain (signature, bitcoinAddress) {
-  let anchor = signature.anchors[0];
-  if (anchor.chain) {
-    let signature = anchor.chain;
-    // TODO put in a separate function (did this way for now since I couldn't find a way to test a private function with Jest, and it's getting late)
-    let chainObject = Object.entries(BLOCKCHAINS).find(entry => entry[1].signatureValue === signature);
-    if (typeof chainObject === 'undefined') {
-      throw new Error('Didn\'t recognize chain value');
+export default function getChain (bitcoinAddress, signature = null) {
+  if (signature == null) {
+    // Legacy path: we didn't support anything other than testnet and mainnet, so we check the address prefix
+    // otherwise try to determine the chain from a bitcoin address
+    return addresses.isMainnet(bitcoinAddress) ? BLOCKCHAINS.bitcoin : BLOCKCHAINS.testnet;
+  } else {
+    let cleanedSignature = signature || {};
+    if (cleanedSignature.anchors) {
+      let anchors = cleanedSignature.anchors;
+      let anchor = anchors[0];
+      let signature = anchor.chain;
+      // TODO put in a separate function (did this way for now since I couldn't find a way to test a private function with Jest, and it's getting late)
+      let chainObject = Object.entries(BLOCKCHAINS).find(entry => entry[1].signatureValue === signature);
+      if (typeof chainObject === 'undefined') {
+        throw new Error('Didn\'t recognize chain value');
+      }
+      return chainObject[1];
     }
-    return chainObject[1];
   }
-  // Legacy path: we didn't support anything other than testnet and mainnet, so we check the address prefix
-  // otherwise try to determine the chain from a bitcoin address
-  return addresses.isMainnet(bitcoinAddress) ? BLOCKCHAINS.bitcoin : BLOCKCHAINS.testnet;
 }
--- test/application/domain/certificates/useCases/getChain.test.js
+++ test/application/domain/certificates/useCases/getChain.test.js
@@ -3,15 +3,26 @@ import { BLOCKCHAINS } from '../../../../../src/index';
 
 describe('domain certificates get chain use case test suite', () => {
   describe('given it is called with a signature', () => {
+    const bitcoinAddress = '';
     const signatureFixture = {
       anchors: [{
         chain: 'bitcoinMainnet'
       }]
     };
-    const result = domain.certificates.getChain(signatureFixture);
+    const result = domain.certificates.getChain(bitcoinAddress, signatureFixture);
     const assertion = BLOCKCHAINS.bitcoin;
 
     it('should return the correct chain object', () => {
+      expect(result).toEqual(assertion);
+    });
+  });
+
+  describe('given it is called without a signature', () => {
+    const bitcoinAddress = '1234567890abcdefghij';
+    const result = domain.certificates.getChain(bitcoinAddress);
+    const assertion = BLOCKCHAINS.bitcoin;
+
+    it('should return the correct the chain object', () => {
       expect(result).toEqual(assertion);
     });
   });

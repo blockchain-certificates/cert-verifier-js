203a3047394ea7ca12a329b9cb89f29014f5dae6
Matthieu COLLÉ <druidev+github@gmail.com>
2018-07-20 20:08:26 -0500

    test(verifier): moved verify unit test to certificate file

--- test/application/certificate.test.js
+++ test/application/certificate.test.js
@@ -1,6 +1,9 @@
 import MainnetV2Certificate from '../fixtures/mainnet-valid-2.0.json';
 import Certificate from '../../src/certificate';
-import { BLOCKCHAINS } from '../../src';
+import { BLOCKCHAINS, VERIFICATION_STATUSES } from '../../src';
+import { readFileAsync } from './utils/readFile';
+import { getVerboseMessage } from '../../config/default';
+import sinon from 'sinon';
 
 describe('Certificate entity test suite', () => {
   describe('constructor method', () => {
@@ -144,6 +147,50 @@ describe('Certificate entity test suite', () => {
           /* eslint no-unused-vars: "off" */
           let certificate = new Certificate();
         }).toThrowError('This is not a valid certificate');
+      });
+    });
+  });
+
+  describe('verify method', () => {
+    xdescribe('the step callback function', () => {
+      let data;
+      let callbackSpy;
+      let testCode;
+      let expectedName;
+      let certificateInstance;
+
+      beforeEach(async () => {
+        data = await readFileAsync('test/fixtures/mocknet-2.0.json');
+        callbackSpy = sinon.spy();
+        certificateInstance = new Certificate(JSON.parse(data));
+
+        testCode = 'getTransactionId';
+        expectedName = getVerboseMessage(testCode);
+      });
+
+      afterEach(() => {
+        data = null;
+        callbackSpy = null;
+
+        testCode = null;
+        expectedName = null;
+      });
+
+      describe('when there is no failure', () => {
+        it('should be called with the code, the name and the status of the step', async () => {
+          await certificateInstance.verify(callbackSpy);
+          // verifierInstance._doAction(testCodefunction () => {});
+          expect(callbackSpy.calledWithExactly(testCode, expectedName, VERIFICATION_STATUSES.SUCCESS, undefined)).toBe(true);
+        });
+      });
+
+      describe('when there is a failure', () => {
+        it('should be called with the code, the name, the status and the error message', async () => {
+          const errorMessage = 'Testing the test';
+          await certificateInstance.verify(callbackSpy);
+          // verifierInstance._doAction(testCodefunction () => { throw new Error(errorMessage); });
+          expect(callbackSpy.calledWithExactly(testCode, expectedName, VERIFICATION_STATUSES.FAILURE, errorMessage)).toBe(true);
+        });
       });
     });
   });

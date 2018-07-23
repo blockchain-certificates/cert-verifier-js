c0734743ee6f294dc497518b141d905a8a62386a
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-23 18:18:12 +0200

    test(Certificate): split test files

--- test/application/certificate/certificate-v2.test.js
+++ test/application/certificate/certificate-v2.test.js
@@ -1,29 +1,10 @@
-import MainnetV2Certificate from '../fixtures/mainnet-valid-2.0.json';
-import Certificate from '../../src/certificate';
-import { BLOCKCHAINS, VERIFICATION_STATUSES } from '../../src';
-import { readFileAsync } from './utils/readFile';
-import { getVerboseMessage } from '../../config/default';
-import sinon from 'sinon';
+import MainnetV2Certificate from '../../fixtures/mainnet-valid-2.0.json';
+import Certificate from '../../../src/certificate';
+import { BLOCKCHAINS } from '../../../src/index';
 
 describe('Certificate entity test suite', () => {
   describe('constructor method', () => {
-    describe('given is is not called with a JSON object', () => {
-      let certificate;
-
-      beforeEach(() => {
-        certificate = new Certificate(JSON.stringify(MainnetV2Certificate));
-      });
-
-      afterEach(() => {
-        certificate = null;
-      });
-
-      it('should coerce certificateJson to an object', () => {
-        expect(certificate.certificateJson).toEqual(MainnetV2Certificate);
-      });
-    });
-
-    describe('given it is called with valid certificate data', () => {
+    describe('given it is called with valid v2 certificate data', () => {
       let certificate;
 
       beforeEach(() => {
@@ -125,66 +106,6 @@ describe('Certificate entity test suite', () => {
         const assertion = Object.assign({}, MainnetV2Certificate);
         delete assertion['signature'];
         expect(certificate.documentToVerify).toEqual(assertion);
-      });
-    });
-
-    describe('given it is called with invalid certificate data', () => {
-      it('should return an error', () => {
-        expect(() => {
-          new Certificate('invalid-certificate-data');
-        }).toThrowError('This is not a valid certificate');
-      });
-    });
-
-    describe('given it is called with no certificate data', () => {
-      it('should throw an error', () => {
-        expect(() => {
-          new Certificate();
-        }).toThrowError('This is not a valid certificate');
-      });
-    });
-  });
-
-  describe('verify method', () => {
-    xdescribe('the step callback function', () => {
-      let data;
-      let callbackSpy;
-      let testCode;
-      let expectedName;
-      let certificateInstance;
-
-      beforeEach(async () => {
-        data = await readFileAsync('test/fixtures/mocknet-2.0.json');
-        callbackSpy = sinon.spy();
-        certificateInstance = new Certificate(JSON.parse(data));
-
-        testCode = 'getTransactionId';
-        expectedName = getVerboseMessage(testCode);
-      });
-
-      afterEach(() => {
-        data = null;
-        callbackSpy = null;
-
-        testCode = null;
-        expectedName = null;
-      });
-
-      describe('when there is no failure', () => {
-        it('should be called with the code, the name and the status of the step', async () => {
-          await certificateInstance.verify(callbackSpy);
-          // verifierInstance._doAction(testCodefunction () => {});
-          expect(callbackSpy.calledWithExactly(testCode, expectedName, VERIFICATION_STATUSES.SUCCESS, undefined)).toBe(true);
-        });
-      });
-
-      describe('when there is a failure', () => {
-        it('should be called with the code, the name, the status and the error message', async () => {
-          const errorMessage = 'Testing the test';
-          await certificateInstance.verify(callbackSpy);
-          // verifierInstance._doAction(testCodefunction () => { throw new Error(errorMessage); });
-          expect(callbackSpy.calledWithExactly(testCode, expectedName, VERIFICATION_STATUSES.FAILURE, errorMessage)).toBe(true);
-        });
       });
     });
   });

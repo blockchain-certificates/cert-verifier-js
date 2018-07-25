a567c4327d4a2651465c8b659f5ca99b971b85b5
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-25 10:20:13 +0200

    refactor(Tests): use proper function to prevent context leaks

--- test/application/certificate/certificate.test.js
+++ test/application/certificate/certificate.test.js
@@ -3,46 +3,48 @@ import { Certificate, VERIFICATION_STATUSES } from '../../../src';
 import { getVerboseMessage } from '../../../config/default';
 import sinon from 'sinon';
 
-describe('Certificate entity test suite', () => {
-  describe('constructor method', () => {
-    describe('given is is not called with a JSON object', () => {
+describe('Certificate entity test suite', function () {
+  describe('constructor method', function () {
+    describe('given it is not called with a JSON object', function () {
       let certificate;
 
-      beforeEach(() => {
+      beforeEach(function () {
         certificate = new Certificate(JSON.stringify(FIXTURES.MainnetV2Valid));
       });
 
-      afterEach(() => {
+      afterEach(function () {
         certificate = null;
       });
 
-      it('should coerce certificateJson to an object', () => {
+      it('should coerce certificateJson to an object', function () {
         expect(certificate.certificateJson).toEqual(FIXTURES.MainnetV2Valid);
       });
     });
 
-    describe('given it is called with invalid certificate data', () => {
-      it('should return an error', () => {
-        expect(() => {
+    describe('given it is called with invalid certificate data', function () {
+      it('should return an error', function () {
+        expect(function () {
           /* eslint no-new: "off" */
           new Certificate('invalid-certificate-data');
         }).toThrowError('This is not a valid certificate');
       });
     });
 
-    describe('given it is called with no certificate data', () => {
-      it('should throw an error', () => {
-        expect(() => {
+    describe('given it is called with no certificate data', function () {
+      it('should throw an error', function () {
+        expect(function () {
           /* eslint no-new: "off" */
           new Certificate();
         }).toThrowError('This is not a valid certificate');
       });
     });
+
+
   });
 
-  describe('verify method', () => {
-    describe('given the callback parameter is passed', () => {
-      describe('when the certificate is valid', () => {
+  describe('verify method', function () {
+    describe('given the callback parameter is passed', function () {
+      describe('when the certificate is valid', function () {
         let finalStep;
         let certificate;
         let callbackSpy = sinon.spy();
@@ -55,23 +57,23 @@ describe('Certificate entity test suite', () => {
           status: VERIFICATION_STATUSES.SUCCESS
         };
 
-        beforeEach(async () => {
+        beforeEach(async function () {
           certificate = new Certificate(FIXTURES.MainnetV2Valid);
         });
 
-        afterEach(() => {
+        afterEach(function () {
           callbackSpy = null;
           certificate = null;
         });
 
-        it('should call it with the step, the text and the status', async () => {
+        it('should call it with the step, the text and the status', async function () {
           finalStep = await certificate.verify(callbackSpy);
           expect(callbackSpy.calledWith(sinon.match(assertionStep))).toBe(true);
           expect(finalStep).toEqual(assertionFinalStep);
         });
       });
 
-      describe('when the certificate is invalid', () => {
+      describe('when the certificate is invalid', function () {
         let certificate;
         let updates = [];
         let assertionStep = {
@@ -81,7 +83,7 @@ describe('Certificate entity test suite', () => {
           errorMessage: 'This certificate has been revoked by the issuer. Reason given: Issued in error.'
         };
 
-        it('should call it with the step, the text, the status & the error message', async () => {
+        it('should call it with the step, the text, the status & the error message', async function () {
           certificate = new Certificate(FIXTURES.MainnetV2Revoked);
           await certificate.verify(update => {
             updates.push(update);

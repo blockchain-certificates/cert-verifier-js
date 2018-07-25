62bda176be3f08d62fc9f411fd9d7eaa8496434e
Julien Fraichot <jfraichot@learningmachine.com>
2018-07-25 13:17:34 +0200

    fix(Certificate): ensure isFailing method returns the truth

--- test/application/certificate/certificate.test.js
+++ test/application/certificate/certificate.test.js
@@ -1,39 +1,62 @@
 import FIXTURES from '../../fixtures';
 import { Certificate } from '../../../src';
+import { VERIFICATION_STATUSES } from '../../../src/constants';
 
-describe('Certificate entity test suite', () => {
-  describe('constructor method', () => {
-    describe('given is is not called with a JSON object', () => {
+describe('Certificate entity test suite', function () {
+  describe('constructor method', function () {
+    describe('given is is not called with a JSON object', function () {
       let certificate;
 
-      beforeEach(() => {
+      beforeEach(function () {
         certificate = new Certificate(JSON.stringify(FIXTURES.MainnetV2Valid));
       });
 
-      afterEach(() => {
+      afterEach(function () {
         certificate = null;
       });
 
-      it('should coerce certificateJson to an object', () => {
+      it('should coerce certificateJson to an object', function () {
         expect(certificate.certificateJson).toEqual(FIXTURES.MainnetV2Valid);
       });
     });
 
-    describe('given it is called with invalid certificate data', () => {
-      it('should return an error', () => {
-        expect(() => {
+    describe('given it is called with invalid certificate data', function () {
+      it('should return an error', function () {
+        expect(function () {
           /* eslint no-new: "off" */
           new Certificate('invalid-certificate-data');
         }).toThrowError('This is not a valid certificate');
       });
     });
 
-    describe('given it is called with no certificate data', () => {
-      it('should throw an error', () => {
-        expect(() => {
+    describe('given it is called with no certificate data', function () {
+      it('should throw an error', function () {
+        expect(function () {
           /* eslint no-new: "off" */
           new Certificate();
         }).toThrowError('This is not a valid certificate');
+      });
+    });
+  });
+
+  describe('isFailing method', function () {
+    describe('when all checks are successful', function () {
+      it('should return false', function () {
+        const certificate = new Certificate(FIXTURES.MainnetV2Valid);
+        certificate._stepsStatuses.push({step: 'testStep 1', status: VERIFICATION_STATUSES.SUCCESS, action: 'Test Step 1'});
+        certificate._stepsStatuses.push({step: 'testStep 2', status: VERIFICATION_STATUSES.SUCCESS, action: 'Test Step 2'});
+
+        expect(certificate._isFailing()).toBe(false);
+      });
+    });
+
+    describe('when one check is failing', function () {
+      it('should return true', function () {
+        const certificate = new Certificate(FIXTURES.MainnetV2Valid);
+        certificate._stepsStatuses.push({step: 'testStep 1', status: VERIFICATION_STATUSES.SUCCESS, action: 'Test Step 1'});
+        certificate._stepsStatuses.push({step: 'testStep 2', status: VERIFICATION_STATUSES.FAILURE, action: 'Test Step 2'});
+
+        expect(certificate._isFailing()).toBe(true);
       });
     });
   });

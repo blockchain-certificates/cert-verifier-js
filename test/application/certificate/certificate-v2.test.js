92b3afd28f393f8558d11d83e08e68423a0bd589
Matthieu COLLÉ <druidev+github@gmail.com>
2018-07-26 20:02:27 -0500

    refactor(networks): initial verification map now based on network

--- test/application/certificate/certificate-v2.test.js
+++ test/application/certificate/certificate-v2.test.js
@@ -1,6 +1,6 @@
 import FIXTURES from '../../fixtures';
 import { Certificate, BLOCKCHAINS } from '../../../src';
-import v20MapAssertion from '../domain/certificates/useCases/assertions/v20MapAssertion';
+import mainnetMapAssertion from '../domain/certificates/useCases/assertions/mainnetMapAssertion';
 
 describe('Certificate entity test suite', function () {
   describe('constructor method', function () {
@@ -95,7 +95,7 @@ describe('Certificate entity test suite', function () {
       });
 
       it('should set verificationSteps to the certificate object', function () {
-        expect(certificate.verificationSteps).toEqual(v20MapAssertion);
+        expect(certificate.verificationSteps).toEqual(mainnetMapAssertion);
       });
     });
   });
--- test/application/domain/certificates/useCases/getVerificationMap.test.js
+++ test/application/domain/certificates/useCases/getVerificationMap.test.js
@@ -1,8 +1,7 @@
 import domain from '../../../../../src/domain';
-import { BLOCKCHAINS, CERTIFICATE_VERSIONS } from '../../../../../src';
+import { BLOCKCHAINS } from '../../../../../src';
 import mocknetMapAssertion from './assertions/mocknetMapAssertion';
-import v12MapAssertion from './assertions/v12MapAssertion';
-import v20MapAssertion from './assertions/v20MapAssertion';
+import mainnetMapAssertion from './assertions/mainnetMapAssertion';
 
 describe('domain certificates get verification map use case test suite', () => {
   describe('given it is called with the mocknet chain', () => {
@@ -13,25 +12,9 @@ describe('domain certificates get verification map use case test suite', () => {
   });
 
   describe('given it is called with the bitcoin chain', () => {
-    describe('given it is called without a version', () => {
-      it('should return a mainnet v2.0 verification map', () => {
-        const result = domain.certificates.getVerificationMap(BLOCKCHAINS.bitcoin);
-        expect(result).toEqual(v20MapAssertion);
-      });
-    });
-
-    describe('given it is called with a v1.2 version', () => {
-      it('should return a mainnet v1.2 verification map', () => {
-        const result = domain.certificates.getVerificationMap(BLOCKCHAINS.bitcoin, CERTIFICATE_VERSIONS.V1_2);
-        expect(result).toEqual(v12MapAssertion);
-      });
-    });
-
-    describe('given it is called with a v2.0 version', () => {
-      it('should return a mainnet v2.0 verification map', () => {
-        const result = domain.certificates.getVerificationMap(BLOCKCHAINS.bitcoin, CERTIFICATE_VERSIONS.V2_0);
-        expect(result).toEqual(v20MapAssertion);
-      });
+    it('should return a mainnet verification map', () => {
+      const result = domain.certificates.getVerificationMap(BLOCKCHAINS.bitcoin);
+      expect(result).toEqual(mainnetMapAssertion);
     });
   });
 

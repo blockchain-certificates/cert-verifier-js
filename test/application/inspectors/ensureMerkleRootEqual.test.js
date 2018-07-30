ab2124ed0173cbf134bdd155072a135712110977
Matthieu COLLÉ <druidev+github@gmail.com>
2018-07-30 16:01:16 -0500

    test(inspectors): added ensureMerkleRootEqual test

---
+++ test/application/inspectors/ensureMerkleRootEqual.test.js
@@ -0,0 +1,23 @@
+import ensureMerkleRootEqual from '../../../src/inspectors/ensureMerkleRootEqual';
+
+describe('Inspectors test suite', function () {
+  describe('ensureMerkleRootEqual method', function () {
+    const errorMessage = 'Merkle root does not match remote hash.';
+
+    describe('given it is called with similar merkleRoot & remoteHash', function () {
+      it('should not throw an error', function () {
+        expect(function () {
+          ensureMerkleRootEqual('similar', 'similar');
+        }).not.toThrow();
+      });
+    });
+
+    describe('given it is called with different merkleRoot & remoteHash', () => {
+      it('should throw an error', function () {
+        expect(function () {
+          ensureMerkleRootEqual('merkle-root', 'remote-hash');
+        }).toThrowError(errorMessage);
+      });
+    });
+  });
+});

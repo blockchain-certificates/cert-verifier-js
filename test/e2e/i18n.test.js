5f650279450de60941dd321d8aeb567bb25d1d62
Julien Fraichot <jfraichot@learningmachine.com>
2019-05-14 20:01:47 +0200

    feat(i18n): update spanish translation

--- test/e2e/i18n.test.js
+++ test/e2e/i18n.test.js
@@ -4,10 +4,10 @@ import assertionSpanishVerificationSteps from '../assertions/assertion-spanish-v
 
 describe('End-to-end i18n test suite', function () {
   describe('given the language is set to spanish', () => {
-    const certificate = new Certificate(FIXTURES.MainnetV2Valid, { locale: 'es-ES' });
+    const certificate = new Certificate(FIXTURES.MainnetV2Valid, { locale: 'es' });
 
-    it('should set the locale to es-ES', async function () {
-      expect(certificate.locale).toBe('es-ES');
+    it('should set the locale to es', async function () {
+      expect(certificate.locale).toBe('es');
     });
 
     it('should have the verification steps in spanish', async function () {

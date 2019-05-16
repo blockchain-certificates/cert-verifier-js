b88397aabd082b5f33d2d7323e67a91b1d1d5c76
Julien Fraichot <jfraichot@learningmachine.com>
2019-05-16 13:43:05 +0200

    chore(Commits): ensure commits follow semantic-release convention

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'scope-empty': [2, 'never'],
+    'scope-case': [2, 'always', ['pascal-case', 'camel-case', 'lower-case']]
+  }
+};

32df951f76d1f52c8e34a6d2b53e95b9b40a3459
Matthieu COLLÉ <druidev+github@gmail.com>
2019-11-13 08:58:40 -0600

    feat(TransactionApis): added new APIs

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,17 +1,44 @@
-const blockCypherUrl = 'https://api.blockcypher.com/v1/btc/main/txs/';
-const blockCypherTestUrl = 'https://api.blockcypher.com/v1/btc/test3/txs/';
-const chainSoUrl = 'https://chain.so/api/v2/get_tx/BTC/';
-const chainSoTestUrl = 'https://chain.so/api/v2/get_tx/BTCTEST/';
-
 // Add &apikey={key} to EtherScan URL's if getting rate limited
 const etherScanMainUrl = 'https://api.etherscan.io/api?module=proxy';
 const etherScanRopstenUrl = 'https://api-ropsten.etherscan.io/api?module=proxy';
 
-export default {
-  blockCypherUrl,
-  blockCypherTestUrl,
-  chainSoUrl,
-  chainSoTestUrl,
+const TRANSACTION_APIS = {
+  Bitcoinchain: 'bitcoinchain',
+  Bitpay: 'bitpay',
+  Blockchaininfo: 'blockchain.info',
+  Blockcypher: 'blockcypher',
+  Blockexplorer: 'Blockexplorer',
+  Blockstream: 'blockstream'
+};
+
+const TRANSACTION_ID_PLACEHOLDER = '{transaction_id}';
+
+const TRANSACTIONS_APIS_URLS = {
+  [TRANSACTION_APIS.Bitcoinchain]: {
+    mainnet: `https://api-r.bitcoinchain.com/v1/tx/${TRANSACTION_ID_PLACEHOLDER}`
+  },
+  [TRANSACTION_APIS.Bitpay]: {
+    mainnet: `https://insight.bitpay.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
+  },
+  [TRANSACTION_APIS.Blockchaininfo]: {
+    mainnet: `https://blockchain.info/rawtx/${TRANSACTION_ID_PLACEHOLDER}`
+  },
+  [TRANSACTION_APIS.Blockcypher]: {
+    mainnet: `https://api.blockcypher.com/v1/btc/main/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`,
+    testnet: `https://api.blockcypher.com/v1/btc/test3/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`
+  },
+  [TRANSACTION_APIS.Blockexplorer]: {
+    mainnet: `https://blockexplorer.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
+  },
+  [TRANSACTION_APIS.Blockstream]: {
+    mainnet: `https://blockstream.info/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
+  }
+};
+
+export {
   etherScanMainUrl,
-  etherScanRopstenUrl
+  etherScanRopstenUrl,
+  TRANSACTION_APIS,
+  TRANSACTIONS_APIS_URLS,
+  TRANSACTION_ID_PLACEHOLDER
 };

e35308895f6519cada2654d6d66ad177b193a127
Matthieu COLLÉ <druidev+github@gmail.com>
2019-11-14 17:13:47 -0600

    feat(Explorers): convert tx time to date object from timestamp

--- src/explorers/bitcoin/apis/blockexplorer.js
+++ src/explorers/bitcoin/apis/blockexplorer.js
@@ -2,12 +2,13 @@ import { BLOCKCHAINS, CONFIG, SUB_STEPS } from '../../../constants';
 import { TransactionData, VerifierError } from '../../../models';
 import { getText } from '../../../domain/i18n/useCases';
 import { stripHashPrefix } from '../../utils/stripHashPrefix';
+import { timestampToDateObject } from '../../../helpers/date';
 
 export function generateTransactionDataFromBlockexplorerResponse (jsonResponse) {
   if (jsonResponse.confirmations < CONFIG.MininumConfirmations) {
     throw new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'parseBlockexplorerResponse'));
   }
-  const time = jsonResponse.blocktime;
+  const time = timestampToDateObject(jsonResponse.blocktime);
   const outputs = jsonResponse.vout;
   const lastOutput = outputs[outputs.length - 1];
   const issuingAddress = jsonResponse.vout[0].scriptPubKey.addresses[0];

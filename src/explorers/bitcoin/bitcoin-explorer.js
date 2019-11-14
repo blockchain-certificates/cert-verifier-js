5196ded7f13d4db57c2859b132b80de712721831
Matthieu COLLÉ <druidev+github@gmail.com>
2019-11-13 21:36:23 -0600

    refactor(BitcoinExplorer): shared explorer

---
+++ src/explorers/bitcoin/bitcoin-explorer.js
@@ -0,0 +1,42 @@
+import { BLOCKCHAINS, SUB_STEPS, TRANSACTION_APIS } from '../../constants';
+import { buildTransactionApiUrl } from '../../services/transaction-apis';
+import { request } from '../../services';
+import { VerifierError } from '../../models';
+import { getText } from '../../domain/i18n/useCases';
+import { generateTransactionDataFromBitpayResponse } from './bitpay';
+import { generateTransactionDataFromBlockcypherResponse } from './blockcypher';
+
+export async function getBitcoinTransactionFromApi (apiName, transactionId, chain) {
+  const isTestnet = chain !== BLOCKCHAINS.bitcoin.code;
+  const requestUrl = buildTransactionApiUrl(apiName, transactionId, isTestnet);
+
+  return new Promise((resolve, reject) => {
+    return request({ url: requestUrl }).then(response => {
+      try {
+        const transactionData = getTransactionDataGeneratorPerApi(apiName)(JSON.parse(response));
+        resolve(transactionData);
+      } catch (err) {
+        reject(err.message);
+      }
+    }).catch(() => {
+      reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
+    });
+  });
+}
+
+const API_TRANSACTION_DATA_GENERATORS = {
+  [TRANSACTION_APIS.Bitcoinchain]: {},
+  [TRANSACTION_APIS.Bitpay]: generateTransactionDataFromBitpayResponse,
+  [TRANSACTION_APIS.Blockchaininfo]: {},
+  [TRANSACTION_APIS.Blockcypher]: generateTransactionDataFromBlockcypherResponse,
+  [TRANSACTION_APIS.Blockexplorer]: {},
+  [TRANSACTION_APIS.Blockstream]: {}
+};
+
+function getTransactionDataGeneratorPerApi (apiName) {
+  const transactionDataGenerator = API_TRANSACTION_DATA_GENERATORS[apiName];
+  if (!transactionDataGenerator) {
+    throw new Error(`API ${apiName} is not listed`);
+  }
+  return transactionDataGenerator;
+}
--- test/application/explorers/bitcoin/bitcoin-explorer.test.js
+++ test/application/explorers/bitcoin/bitcoin-explorer.test.js
@@ -1,9 +1,10 @@
-import { getBitpayTransaction } from '../../../src/explorers/bitcoin/bitpay';
 import sinon from 'sinon';
-import * as RequestService from '../../../src/services/request';
-import * as mockBitpayResponse from './mocks/mockBitpayResponse';
+import * as RequestService from '../../../../src/services';
+import * as mockBitpayResponse from '../mocks/mockBitpayResponse';
+import { getBitcoinTransactionFromApi } from '../../../../src/explorers/bitcoin/bitcoin-explorer';
+import { TRANSACTION_APIS } from '../../../../src/constants';
 
-describe('BitPay class test suite', function () {
+describe('Bitcoin Explorer test suite', function () {
   const fixtureTransactionId = '2378076e8e140012814e98a2b2cb1af07ec760b239c1d6d93ba54d658a010ecd';
   const assertionRequestUrl = `https://insight.bitpay.com/api/tx/${fixtureTransactionId}`;
   let stubRequest;
@@ -22,9 +23,9 @@ describe('BitPay class test suite', function () {
     stubRequest.restore();
   });
 
-  describe('getBitpayTransaction method', function () {
+  describe('getBitcoinTransactionFromApi method', function () {
     it('should call the right request API', function () {
-      getBitpayTransaction(fixtureTransactionId).then(() => {
+      getBitcoinTransactionFromApi(TRANSACTION_APIS.Bitpay, fixtureTransactionId).then(() => {
         expect(stubRequest.getCall(0).args).toEqual([{ url: assertionRequestUrl }]);
       });
     });
@@ -33,7 +34,7 @@ describe('BitPay class test suite', function () {
       it('should throw the right error', async function () {
         const fixtureError = new Error('Unable to get remote hash');
         stubRequest.rejects(fixtureError);
-        await getBitpayTransaction(fixtureTransactionId).catch(err => {
+        await getBitcoinTransactionFromApi(TRANSACTION_APIS.Bitpay, fixtureTransactionId).catch(err => {
           expect(err).toEqual(fixtureError);
         });
       });
@@ -42,19 +43,9 @@ describe('BitPay class test suite', function () {
     describe('given the request is successful', function () {
       describe('and the transaction data is generated from the response', function () {
         it('should return a correct transaction data', async function () {
-          getBitpayTransaction(fixtureTransactionId).then(res => {
+          getBitcoinTransactionFromApi(TRANSACTION_APIS.Bitpay, fixtureTransactionId).then(res => {
             expect(res).toEqual(assertionResponse);
           });
-        });
-      });
-
-      describe('and the transaction does not have enough confirmations yet', function () {
-        it('should throw the right error', async function () {
-          stubRequest.resolves(JSON.stringify({
-            ...mockBitpayResponse,
-            confirmations: 0
-          }));
-          await expect(getBitpayTransaction(fixtureTransactionId)).rejects.toEqual(new Error('Number of transaction confirmations were less than the minimum required, according to Bitpay API'));
         });
       });
     });

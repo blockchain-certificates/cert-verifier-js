b45a09beca4e93a83f10f6dbbeded0e6c0c58195
Matthieu COLLÉ <druidev+github@gmail.com>
2020-01-21 12:44:13 -0600

    refactor(BlockchainExplorer): parameters now object

--- src/services/transaction-apis.js
+++ src/services/transaction-apis.js
@@ -1,11 +1,10 @@
-import { TRANSACTION_ID_PLACEHOLDER } from '../constants';
 import { TRANSACTIONS_APIS_URLS } from '../constants/api';
 
-export function buildTransactionApiUrl (apiName, transactionId, testApi = false) {
+export function buildTransactionApiUrl ({ apiName, searchValue = '', newValue = '', testApi = false }) {
   const api = TRANSACTIONS_APIS_URLS[apiName];
   if (!api) {
     throw new Error(`API ${apiName} is not listed`);
   }
-  const apiUrl = testApi ? api.testnet : api.mainnet;
-  return apiUrl.replace(TRANSACTION_ID_PLACEHOLDER, transactionId);
+  const apiUrl = testApi ? api.test : api.main;
+  return apiUrl.replace(searchValue, newValue);
 }

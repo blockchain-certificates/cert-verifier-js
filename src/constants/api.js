94d34a1e04847b97db3269ef72939fd173b1ef0d
Matthieu COLLÉ <druidev+github@gmail.com>
2020-01-21 12:45:39 -0600

    feat(Constants): [#2688] added isTestChain method

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,39 +1,58 @@
+import { BLOCKCHAINS } from './blockchains';
+
 const TRANSACTION_APIS = {
   Bitpay: 'bitpay',
   Blockcypher: 'blockcypher',
   Blockexplorer: 'blockexplorer',
   Blockstream: 'blockstream',
-  Etherscan: 'etherscan'
+  Etherscan: 'etherscan',
+  EtherscanBlockNumber: 'etherscanBlockNumber',
+  EtherscanScanBlock: 'etherscanScanBlock'
 };
 
 const TRANSACTION_ID_PLACEHOLDER = '{transaction_id}';
+const BLOCK_NUMBER_PLACEHOLDER = '{block_number}';
 
 const TRANSACTIONS_APIS_URLS = {
   [TRANSACTION_APIS.Bitpay]: {
-    mainnet: `https://insight.bitpay.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
-    testnet: `https://api.bitcore.io/api/BTC/testnet/tx/${TRANSACTION_ID_PLACEHOLDER}`
+    main: `https://insight.bitpay.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
+    test: `https://api.bitcore.io/api/BTC/testnet/tx/${TRANSACTION_ID_PLACEHOLDER}`
   },
   [TRANSACTION_APIS.Blockcypher]: {
-    mainnet: `https://api.blockcypher.com/v1/btc/main/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`,
-    testnet: `https://api.blockcypher.com/v1/btc/test3/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`
+    main: `https://api.blockcypher.com/v1/btc/main/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`,
+    test: `https://api.blockcypher.com/v1/btc/test3/txs/${TRANSACTION_ID_PLACEHOLDER}?limit=500`
   },
   [TRANSACTION_APIS.Blockexplorer]: {
-    mainnet: `https://blockexplorer.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
-    testnet: `https://testnet.blockexplorer.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
+    main: `https://blockexplorer.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
+    test: `https://testnet.blockexplorer.com/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
   },
   [TRANSACTION_APIS.Blockstream]: {
-    mainnet: `https://blockstream.info/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
-    testnet: `https://blockstream.info/testnet/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
+    main: `https://blockstream.info/api/tx/${TRANSACTION_ID_PLACEHOLDER}`,
+    test: `https://blockstream.info/testnet/api/tx/${TRANSACTION_ID_PLACEHOLDER}`
   },
   // Add &apikey={key} to EtherScan URL's if getting rate limited
   [TRANSACTION_APIS.Etherscan]: {
-    main: `https://api.etherscan.io/api?module=proxy`,
-    ropsten: `https://api-ropsten.etherscan.io/api?module=proxy`
+    main: `https://api.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${TRANSACTION_ID_PLACEHOLDER}`,
+    test: `https://api-ropsten.etherscan.io/api?module=proxy&action=eth_getTransactionByHash&txhash=${TRANSACTION_ID_PLACEHOLDER}`
+  },
+  [TRANSACTION_APIS.EtherscanBlockNumber]: {
+    main: `https://api.etherscan.io/api?module=proxy&action=eth_blockNumber`,
+    test: `https://api-ropsten.etherscan.io/api?module=proxy&action=eth_blockNumber`
+  },
+  [TRANSACTION_APIS.EtherscanScanBlock]: {
+    main: `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&boolean=true&tag=${BLOCK_NUMBER_PLACEHOLDER}`,
+    test: `https://api-ropsten.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&boolean=true&tag=${BLOCK_NUMBER_PLACEHOLDER}`
   }
 };
+
+function isTestChain (chain) {
+  return chain !== BLOCKCHAINS.bitcoin.code && chain !== BLOCKCHAINS.ethmain.code;
+}
 
 export {
   TRANSACTION_APIS,
   TRANSACTIONS_APIS_URLS,
-  TRANSACTION_ID_PLACEHOLDER
+  TRANSACTION_ID_PLACEHOLDER,
+  BLOCK_NUMBER_PLACEHOLDER,
+  isTestChain
 };

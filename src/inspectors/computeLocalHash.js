e2379cb81027f624321bb3b52f92a7a86c46fffa
Julien Fraichot <jfraichot@learningmachine.com>
2020-02-03 12:18:15 -0600

    feat(Verify): set V3 contexts locally

--- src/inspectors/computeLocalHash.js
+++ src/inspectors/computeLocalHash.js
@@ -12,7 +12,10 @@ const {
   obi: OBI_CONTEXT,
   blockcerts: BLOCKCERTS_CONTEXT,
   blockcertsv1_2: BLOCKCERTSV1_2_CONTEXT,
-  blockcertsv2: BLOCKCERTSV2_CONTEXT
+  blockcertsv2: BLOCKCERTSV2_CONTEXT,
+  blockcertsV3: BLOCKCERTSV3_CONTEXT,
+  vc: VERIFIABLE_CRED_CTX,
+  vc_exp: VERIFIABLE_CRED_EXAMPLE
 } = ContextsMap;
 const CONTEXTS = {};
 // Preload contexts
@@ -23,6 +26,11 @@ CONTEXTS['https://openbadgespec.org/v2/context.json'] = OBI_CONTEXT;
 CONTEXTS['https://w3id.org/blockcerts/v2'] = BLOCKCERTSV2_CONTEXT;
 CONTEXTS['https://www.w3id.org/blockcerts/schema/2.0/context.json'] = BLOCKCERTSV2_CONTEXT;
 CONTEXTS['https://w3id.org/blockcerts/v1'] = BLOCKCERTSV1_2_CONTEXT;
+
+// V3
+CONTEXTS['https://www.blockcerts.org/schema/3.0-alpha/context.json'] = BLOCKCERTSV3_CONTEXT;
+CONTEXTS['https://www.w3.org/2018/credentials/v1'] = VERIFIABLE_CRED_CTX;
+CONTEXTS['https://www.w3.org/2018/credentials/examples/v1'] = VERIFIABLE_CRED_EXAMPLE;
 
 function getUnmappedFields (normalized) {
   const myRegexp = /<http:\/\/fallback\.org\/(.*)>/;
@@ -47,6 +55,7 @@ export default function computeLocalHash (document, version) {
       expandContext.push({ '@vocab': 'http://fallback.org/' });
     }
   }
+
   const nodeDocumentLoader = jsonld.documentLoaders.node();
   const customLoader = function (url, callback) {
     if (url in CONTEXTS) {
@@ -61,7 +70,8 @@ export default function computeLocalHash (document, version) {
   jsonld.documentLoader = customLoader;
   let normalizeArgs = {
     algorithm: 'URDNA2015',
-    format: 'application/nquads'
+    format: 'application/nquads',
+    // protectedMode: 'warn'
   };
   if (expandContext) {
     normalizeArgs.expandContext = expandContext;
@@ -69,6 +79,7 @@ export default function computeLocalHash (document, version) {
 
   return new Promise((resolve, reject) => {
     jsonld.normalize(theDocument, normalizeArgs, (err, normalized) => {
+      console.log(err);
       const isErr = !!err;
       if (isErr) {
         reject(
@@ -84,6 +95,7 @@ export default function computeLocalHash (document, version) {
             )
           ); // + unmappedFields.join(",")
         } else {
+          console.log(normalized);
           resolve(sha256(toUTF8Data(normalized)));
         }
       }

df5099e5bbef3bc7a280bbea35dae85431f6b54b
Julien Fraichot <jfraichot@learningmachine.com>
2020-02-17 15:22:55 +0100

    test(Build): remove jest error with todo test

--- test/build/verifier-iife.test.build.js
+++ test/build/verifier-iife.test.build.js
@@ -1,14 +1,8 @@
 /* eslint no-undef: 0 */
 // TODO remove above eslint rule
 import '../../dist/verifier-iife';
-import { VERIFICATION_STATUSES } from '../../src';
-import FIXTURES from '../fixtures';
 
 // TODO: this test would be better as a browser based (ie: run mocha in a browser test suite, like we do in Blockcerts Verifier)
 describe('verifier build test suite', function () {
-  it.todo('works as expected', async function () {
-    const certificate = new Verifier.Certificate(FIXTURES.MainnetV2Valid);
-    const result = await certificate.verify();
-    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
-  });
+  it.todo('works as expected');
 });

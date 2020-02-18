d6177c2d24d934635b1c615d9ca1e41de82eec43
Julien Fraichot <jfraichot@learningmachine.com>
2020-02-18 15:59:34 +0100

    test(Build): test iife build

--- test/build/verifier-iife.test.build.js
+++
@@ -1,8 +0,0 @@
-/* eslint no-undef: 0 */
-// TODO remove above eslint rule
-import '../../dist/verifier-iife';
-
-// TODO: this test would be better as a browser based (ie: run mocha in a browser test suite, like we do in Blockcerts Verifier)
-describe('verifier build test suite', function () {
-  it.todo('works as expected');
-});

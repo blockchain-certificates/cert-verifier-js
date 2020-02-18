fc2a48b4f8d7049fad75f425f2c5f1353a240b7e
Julien Fraichot <jfraichot@learningmachine.com>
2020-02-18 19:04:48 +0100

    test(Build): run iife tests with headless browsers

--- karma.conf.js
+++ karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function (config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox', 'Chrome'],
+    browsers: ['FirefoxHeadless', 'ChromeHeadless'],
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits

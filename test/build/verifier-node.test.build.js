e475ab4e1bbf6a26e1dbac6da1c8301f7bff1a4c
Julien Fraichot <jfraichot@learningmachine.com>
2020-03-18 14:53:47 +0100

    chore(Node): add node build step

---
+++ test/build/verifier-node.test.build.js
@@ -0,0 +1,11 @@
+import { VERIFICATION_STATUSES } from '../../src';
+import FIXTURES from '../fixtures';
+const verifier = require('../../lib');
+
+describe('verifier build test suite', function () {
+  it('works as expected', async function () {
+    const certificate = new verifier.Certificate(FIXTURES.MainnetV2Valid);
+    const result = await certificate.verify();
+    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
+  });
+});

d4b387243da15a21680d512c478de17ad2387b07
Julien Fraichot <jfraichot@learningmachine.com>
2020-04-09 16:22:56 +0200

    perf(Tests): [#479] stub txData

--- test/e2e/verifier/testnet-v2-issuer-v1.test.js
+++ test/e2e/verifier/testnet-v2-issuer-v1.test.js
@@ -1,11 +1,22 @@
 import { Certificate, VERIFICATION_STATUSES } from '../../../src';
 import FIXTURES from '../../fixtures';
+import sinon from 'sinon';
+import * as bitcoinExplorer from '../../../src/explorers/bitcoin/bitcoin-explorer';
 
 describe('given the certificate is a valid testnet (v2.0) issued by v1 issuer', function () {
   it('should verify successfully', async function () {
+    sinon.stub(bitcoinExplorer, 'getBitcoinTransactionFromApi').resolves({
+      remoteHash: '4bc7314351723d1670ff49250aada05d6e5da31d0369a999f8e3cbc7fede5b74',
+      issuingAddress: 'msBCHdwaQ7N2ypBYupkp6uNxtr9Pg76imj',
+      time: '2017-05-03T18:29:43.000Z',
+      revokedAddresses: [
+        'msBCHdwaQ7N2ypBYupkp6uNxtr9Pg76imj'
+      ]
+    });
     const certificate = new Certificate(FIXTURES.TestnetV2ValidV1Issuer);
     await certificate.init();
     const result = await certificate.verify();
     expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
+    sinon.restore();
   });
 });

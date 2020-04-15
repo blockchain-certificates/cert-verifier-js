f38ea7006249693199978e3d8afde6f2fe6da8d6
Matthieu COLLÉ <druidev+github@gmail.com>
2020-04-15 15:38:45 -0500

    chore(ApiConstants): reverted to original etherscan url

--- src/constants/api.js
+++ src/constants/api.js
@@ -7,13 +7,12 @@ const TRANSACTION_APIS = {
 };
 
 const TRANSACTION_ID_PLACEHOLDER = '{transaction_id}';
-
 const ETHERSCAN_API_KEY = 'FJ3CZWH8PQBV8W5U6JR8TMKAYDHBKQ3B1D';
 
 const TRANSACTIONS_APIS_URLS = {
   [TRANSACTION_APIS.Etherscan]: {
-    main: `https://api.etherscan.io/api?module=proxy&apikey=${ETHERSCAN_API_KEY}&action=eth_getTransactionByHash&txhash=${TRANSACTION_ID_PLACEHOLDER}`,
-    test: `https://api-ropsten.etherscan.io/api?module=proxy&apikey=${ETHERSCAN_API_KEY}&action=eth_getTransactionByHash&txhash=${TRANSACTION_ID_PLACEHOLDER}`
+    main: `https://api.etherscan.io/api?module=proxy&apikey=${ETHERSCAN_API_KEY}`,
+    test: `https://api-ropsten.etherscan.io/api?module=proxy&apikey=${ETHERSCAN_API_KEY}`
   }
 };
 

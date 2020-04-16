15768a4326b63283976c128d1b78b9560748d5ba
Julien Fraichot <jfraichot@learningmachine.com>
2020-04-16 15:19:17 +0200

    refactor(Types): [#491] add typings to TransactionData

--- src/models/transactionData.js
+++
@@ -1,8 +0,0 @@
-export default class TransactionData {
-  constructor (remoteHash, issuingAddress, time, revokedAddresses) {
-    this.remoteHash = remoteHash;
-    this.issuingAddress = issuingAddress;
-    this.time = time;
-    this.revokedAddresses = revokedAddresses;
-  }
-}

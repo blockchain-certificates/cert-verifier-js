77e94a8cbe87ab3d6970cdc4df3ee5ab12b9433c
Matthieu COLLÉ <druidev+github@gmail.com>
2020-04-17 10:18:43 -0500

    refactor(Etherscan): improved error handling

--- src/explorers/ethereum/etherscan.js
+++ src/explorers/ethereum/etherscan.js
@@ -1,4 +1,4 @@
-import { request } from '../../services';
+import { request } from '../../services/request';
 import { BLOCKCHAINS, CONFIG, SUB_STEPS, TRANSACTION_ID_PLACEHOLDER } from '../../constants';
 import { generateTransactionData, VerifierError } from '../../models';
 import { stripHashPrefix } from '../utils/stripHashPrefix';
@@ -33,74 +33,54 @@ function parseEtherScanResponse (jsonResponse, block) {
   return generateTransactionData(opReturnScript, issuingAddress, date, undefined);
 }
 
-function getEtherScanBlock (jsonResponse, chain) {
+async function getEtherScanBlock (jsonResponse, chain) {
   const data = jsonResponse.result;
   const blockNumber = data.blockNumber;
   const requestUrl = buildTransactionServiceUrl({
     serviceUrls: getBlockByNumberServiceUrls,
-    searchValue: TRANSACTION_ID_PLACEHOLDER,
-    newValue: blockNumber,
+    transactionIdPlaceholder: TRANSACTION_ID_PLACEHOLDER,
+    transactionId: blockNumber,
     testApi: isTestChain(chain)
   });
 
-  return new Promise((resolve, reject) => {
-    return request({ url: requestUrl })
-      .then(function (response) {
-        const responseData = JSON.parse(response);
-        const blockData = responseData.result;
-        try {
-          const checkConfirmationsFetcher = checkEtherScanConfirmations(chain, blockNumber);
-          checkConfirmationsFetcher
-            .then(function () {
-              resolve(blockData);
-            })
-            .catch(function () {
-              reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
-            });
-        } catch (err) {
-          // don't need to wrap this exception
-          reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
-        }
-      }).catch(function () {
-        reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
-      });
-  });
+  try {
+    const response = await request({ url: requestUrl });
+    const responseData = JSON.parse(response);
+    const blockData = responseData.result;
+
+    await checkEtherScanConfirmations(chain, blockNumber);
+    return blockData;
+  } catch (err) {
+    throw new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash'));
+  }
 }
 
-function checkEtherScanConfirmations (chain, blockNumber) {
+async function checkEtherScanConfirmations (chain, blockNumber) {
   const requestUrl = buildTransactionServiceUrl({
     serviceUrls: getBlockNumberServiceUrls,
     testApi: isTestChain(chain)
   });
 
-  return new Promise((resolve, reject) => {
-    return request({ url: requestUrl })
-      .then(function (response) {
-        const responseData = JSON.parse(response);
-        const currentBlockCount = responseData.result;
-        try {
-          if (currentBlockCount - blockNumber < CONFIG.MininumConfirmations) {
-            reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'checkEtherScanConfirmations')));
-          }
-          resolve(currentBlockCount);
-        } catch (err) {
-          // don't need to wrap this exception
-          reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
-        }
-      }).catch(function () {
-        reject(new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash')));
-      });
-  });
+  let response;
+  try {
+    response = await request({ url: requestUrl });
+  } catch (err) {
+    throw new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'unableToGetRemoteHash'));
+  }
+
+  const responseData = JSON.parse(response);
+  const currentBlockCount = responseData.result;
+
+  if (currentBlockCount - blockNumber < CONFIG.MininumConfirmations) {
+    throw new VerifierError(SUB_STEPS.fetchRemoteHash, getText('errors', 'checkEtherScanConfirmations'));
+  }
+  return currentBlockCount;
 }
 
-function parsingTransactionDataFunction (jsonResponse, chain) {
+async function parsingTransactionDataFunction (jsonResponse, chain) {
   // Parse block to get timestamp first, then create TransactionData
-  const blockFetcher = getEtherScanBlock(jsonResponse, chain);
-  blockFetcher.then(function (blockResponse) {
-    return parseEtherScanResponse(jsonResponse, blockResponse);
-  }).catch(function () {
-    throw new Error(getText('errors', 'unableToGetRemoteHash'));
-  });
+  const blockResponse = await getEtherScanBlock(jsonResponse, chain);
+  return parseEtherScanResponse(jsonResponse, blockResponse);
 }
 
 export {

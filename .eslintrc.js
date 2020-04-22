baf75e4f9db37c6a06f2da57050990fc2064307a
Matthieu COLLÉ <druidev+github@gmail.com>
2020-04-22 14:35:46 -0500

    chore(Lint): added typescript-eslint semi config

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,13 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
   ],
-  'parserOptions': {
-    'project': './tsconfig.json'
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  rules: {
+    'semi': 'off',
+    '@typescript-eslint/semi': ['error', 'always'],
+    '@typescript-eslint/no-empty-function': ['error', { 'allow': ['arrowFunctions'] }],
+    '@typescript-eslint/member-delimiter-style': ['error', { 'multiline': { 'delimiter': 'semi', 'requireLast': true }}]
   }
 };

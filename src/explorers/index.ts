99145197fd75fb327407446cec261c3d0c9988d2
Julien Fraichot <jfraichot@learningmachine.com>
2020-06-02 16:58:18 +0200

    fix(ExplorerAPIs): [#548] throw error early when overwriting explorer is malformed

--- src/explorers/index.ts
+++ src/explorers/index.ts
@@ -17,6 +17,14 @@ export interface TDefaultExplorersPerBlockchain {
 function cleanupExplorerAPIs (explorerAPIs: ExplorerAPI[], indexes: number[]): void {
   indexes.forEach(index => explorerAPIs.splice(index, 1)); // remove modified explorer to avoid setting them in the custom option later
 }
+
+function validateOverwritingExplorer (explorerAPI: ExplorerAPI): boolean {
+  if (explorerAPI.key && !explorerAPI.keyPropertyName) {
+    throw new Error(`Property keyPropertyName is not set for ${explorerAPI.serviceName}. Cannot pass the key property to the service.`);
+  }
+  return true;
+}
+
 const overwrittenIndexes: number[] = [];
 
 export function overwriteDefaultExplorers (explorerAPIs: ExplorerAPI[] = [], defaultExplorers: ExplorerAPI[] = [], lastSetOfExplorers = false): ExplorerAPI[] {
@@ -30,11 +38,13 @@ export function overwriteDefaultExplorers (explorerAPIs: ExplorerAPI[] = [], def
       if (userSetExplorerAPIsName.includes(defaultExplorerAPI.serviceName)) {
         const immutableExplorerAPI = Object.assign({}, defaultExplorerAPI);
         const customSetExplorerAPI = explorerAPIs.find(customExplorerAPI => customExplorerAPI.serviceName === defaultExplorerAPI.serviceName);
-        const overwrittenExplorerAPI = Object.assign(immutableExplorerAPI, customSetExplorerAPI);
-        overwrittenExplorers.push(overwrittenExplorerAPI);
-        const explorerAPIsIndex = explorerAPIs.findIndex(explorerAPI => explorerAPI.serviceName === overwrittenExplorerAPI.serviceName);
-        if (!overwrittenIndexes.includes(explorerAPIsIndex)) {
-          overwrittenIndexes.push(explorerAPIsIndex);
+        if (validateOverwritingExplorer(customSetExplorerAPI)) {
+          const overwrittenExplorerAPI = Object.assign(immutableExplorerAPI, customSetExplorerAPI);
+          overwrittenExplorers.push(overwrittenExplorerAPI);
+          const explorerAPIsIndex = explorerAPIs.findIndex(explorerAPI => explorerAPI.serviceName === overwrittenExplorerAPI.serviceName);
+          if (!overwrittenIndexes.includes(explorerAPIsIndex)) {
+            overwrittenIndexes.push(explorerAPIsIndex);
+          }
         }
       } else {
         overwrittenExplorers.push(defaultExplorerAPI);

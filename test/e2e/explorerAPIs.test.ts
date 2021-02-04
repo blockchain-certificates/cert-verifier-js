1ac8cf7bfae8e3ca1a2b1f923174aa0638e6bfd9
Julien Fraichot <jfraichot@learningmachine.com>
2021-02-04 17:24:07 +0100

    feat(LookForTx): use from explorer-lookup package

--- test/e2e/explorerAPIs.test.ts
+++ test/e2e/explorerAPIs.test.ts
@@ -1,7 +1,7 @@
 import sinon from 'sinon';
 import Certificate, { ExplorerAPI } from '../../src/certificate';
-import * as requestService from '../../src/services/request';
 import FIXTURES from '../fixtures';
+import * as RequestService from '@blockcerts/explorer-lookup/lib/cjs/services/request.js';
 
 describe('explorerAPIs end to end test suite', function () {
   describe('given a custom explorer API with a parsingFunction is set', function () {
@@ -13,7 +13,6 @@ describe('explorerAPIs end to end test suite', function () {
           time: '2018-06-01T20:47:55.000Z',
           revokedAddresses: []
         });
-        sinon.stub(requestService, 'request').resolves('{}');
         const explorerAPI: ExplorerAPI = {
           serviceURL: {
             main: 'https://blockcerts.org/test',
@@ -23,40 +22,12 @@ describe('explorerAPIs end to end test suite', function () {
           parsingFunction: parsingFunctionStub
         };
 
+        sinon.stub(RequestService, 'request').resolves('{}');
+
         const instance = new Certificate(FIXTURES.EthereumMainV2Valid, { explorerAPIs: [explorerAPI] });
         await instance.init();
         await instance.verify();
         expect(parsingFunctionStub.calledOnce).toBe(true);
-        sinon.restore();
-      });
-    });
-  });
-
-  describe('given a custom explorer API with an identifying key is set', function () {
-    describe('and the verification process occurs', function () {
-      it('should call the explorer URL with the identifying information', async function () {
-        const parsingFunctionStub: sinon.SinonStub = sinon.stub().resolves({
-          remoteHash: 'ec049a808a09f3e8e257401e0898aa3d32a733706fd7d16aacf0ba95f7b42c0c',
-          issuingAddress: '0x3d995ef85a8d1bcbed78182ab225b9f88dc8937c',
-          time: '2018-06-01T20:47:55.000Z',
-          revokedAddresses: []
-        });
-        const requestStub = sinon.stub(requestService, 'request').resolves('{}');
-        const explorerAPI: ExplorerAPI = {
-          serviceURL: {
-            main: 'https://blockcerts.org/test',
-            test: 'https://blockcerts.org/test'
-          },
-          priority: 0,
-          parsingFunction: parsingFunctionStub,
-          key: 'user-id',
-          keyPropertyName: 'identification-key'
-        };
-
-        const instance = new Certificate(FIXTURES.EthereumMainV2Valid, { explorerAPIs: [explorerAPI] });
-        await instance.init();
-        await instance.verify();
-        expect(requestStub.getCall(0).args[0].url).toBe('https://blockcerts.org/test?identification-key=user-id');
         sinon.restore();
       });
     });
--- test/e2e/verifier/ethereum-main-invalid-merkle-root.test.ts
+++ test/e2e/verifier/ethereum-main-invalid-merkle-root.test.ts
@@ -1,14 +1,14 @@
 import { Certificate, VERIFICATION_STATUSES } from '../../../src';
 import sinon from 'sinon';
 import FIXTURES from '../../fixtures';
-import * as Explorers from '../../../src/explorers/explorer';
+import domain from '../../../src/domain';
 
 describe('given the certificate is an ethereum main with an invalid merkle root', function () {
   let certificate;
   let result;
 
   beforeAll(async function () {
-    sinon.stub(Explorers, 'getTransactionFromApi').resolves({
+    sinon.stub(domain.verifier, 'lookForTx').resolves({
       remoteHash: '4f48e91f0397a49a5b56718a78d681c51932c8bd9242442b94bcfb93434957db',
       issuingAddress: '0x3d995ef85a8d1bcbed78182ab225b9f88dc8937c',
       time: '2018-05-08T18:30:34.000Z',

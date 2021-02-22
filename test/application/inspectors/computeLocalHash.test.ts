17aa6d486f395c4552421ed2e8e455d6476c364e
Julien Fraichot <jfraichot@learningmachine.com>
2021-02-22 15:59:47 +0100

    test(computeLocalHash): compute hash as in real life usage

--- test/application/inspectors/computeLocalHash.test.ts
+++ test/application/inspectors/computeLocalHash.test.ts
@@ -3,9 +3,14 @@ import computeLocalHash from '../../../src/inspectors/computeLocalHash';
 import Versions from '../../../src/constants/certificateVersions';
 
 describe('computeLocalHash test suite', function () {
+  let fixture;
+  beforeEach(function () {
+    fixture = Object.assign({}, blockcertsV3Fixture);
+    delete fixture.proof;
+  });
+
   describe('given it receives a document', function () {
     it('should return the SHA-256 hashed version', async function () {
-      const fixture = JSON.parse(JSON.stringify(blockcertsV3Fixture));
       const output = await computeLocalHash(fixture, Versions.V3_0_alpha);
       expect(output).toBe('5a44e794431569f4b50a44336c3d445085f09ac5785e38e133385fb486ada9c5');
     });
@@ -13,7 +18,6 @@ describe('computeLocalHash test suite', function () {
 
   describe('given it receives the document has unmapped fields', function () {
     it('should throw', async function () {
-      const fixture = JSON.parse(JSON.stringify(blockcertsV3Fixture));
       fixture.testUnmapped = 'this field is not mapped';
       await expect(async () => {
         await computeLocalHash(fixture, Versions.V3_0_alpha);

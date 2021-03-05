2e4f624c2f21435a3e823665855a8df4c4b9c680
Matthieu COLLÉ <druidev+github@gmail.com>
2021-03-05 13:22:29 -0500

    chore(linting): definition order in RevokedAssertions

--- src/models/RevokedAssertions.ts
+++ src/models/RevokedAssertions.ts
@@ -1,11 +1,11 @@
+export interface RevokedAssertion {
+  id: string;
+  revocationReason: string;
+}
+
 export interface RevocationList {
   type: string | any[];
   id: string;
   issuer: string;
   revokedAssertions: RevokedAssertion[];
-}
-
-export interface RevokedAssertion {
-  id: string;
-  revocationReason: string;
 }

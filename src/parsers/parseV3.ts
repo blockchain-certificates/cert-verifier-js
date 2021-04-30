d14d026f960c2258af5605372c24e7e195d1224c
Julien Fraichot <julien.fraichot@hyland.com>
2021-04-30 15:59:58 +0200

    feat(v3b): add v3 beta parsing support

--- src/parsers/parseV3.ts
+++ src/parsers/parseV3.ts
@@ -2,25 +2,28 @@ import { Decoder } from '@vaultie/lds-merkle-proof-2019';
 import { CERTIFICATE_VERSIONS } from '../constants';
 import domain from '../domain';
 import { BlockcertsV3 } from '../models/BlockcertsV3';
+import { Issuer } from '../models/Issuer';
+import { ProofValue } from '../models/MerkleProof2019';
 
-function parseSignature (signature): any { // TODO: define v3 signature type
+function parseSignature (signature): ProofValue {
   const base58Decoder = new Decoder(signature.proofValue);
   return base58Decoder.decode();
 }
 
 function getRecipientFullName (certificateJson): string {
   const { credentialSubject } = certificateJson;
-  return credentialSubject.name;
+  return credentialSubject.name || '';
 }
 
-async function getIssuerProfile (issuer): Promise<any> { // TODO: define issuer profile
+async function getIssuerProfile (issuer): Promise<Issuer> {
   const profile = await domain.verifier.getIssuerProfile(issuer);
   return profile;
 }
 
 export default async function parseV3 (certificateJson): Promise<BlockcertsV3> {
   const receipt = parseSignature(certificateJson.proof);
-  const { issuer: issuerProfileUrl, metadataJson, issuanceDate, id, expirationDate } = certificateJson;
+  const { issuer: issuerProfileUrl, metadataJson, metadata, issuanceDate, id, expirationDate } = certificateJson;
+  const certificateMetadata = metadata || metadataJson;
   const issuer = await getIssuerProfile(issuerProfileUrl);
   return {
     chain: domain.certificates.getChain('', receipt),
@@ -28,7 +31,7 @@ export default async function parseV3 (certificateJson): Promise<BlockcertsV3> {
     issuedOn: issuanceDate,
     id,
     issuer,
-    metadataJson,
+    metadataJson: certificateMetadata,
     receipt,
     recipientFullName: getRecipientFullName(certificateJson),
     recordLink: id,

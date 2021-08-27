408c5bcac19c140432b22a81b4842a31311baedc
Julien Fraichot <julien.fraichot@hyland.com>
2021-08-27 15:40:54 +0200

    test(Build): adapt build tests to v1 deprecation

--- src/parsers/parseV1.ts
+++ src/parsers/parseV1.ts
@@ -12,6 +12,7 @@ export default function parseV1 (): void {
     '',
     'Verification of v1 certificates is not supported by this component. ' +
     'See the python cert-verifier for v1.1 verification ' +
-    'or the npm package cert-verifier-js-v1-legacy for v1.2 (https://www.npmjs.com/package/@blockcerts/cert-verifier-js-v1-legacy)'
+    'or the npm package cert-verifier-js-v1-legacy for v1.2 ' +
+    '(https://www.npmjs.com/package/@blockcerts/cert-verifier-js-v1-legacy)'
   );
 }
--- test/build/verifier.test.build.js
+++ test/build/verifier.test.build.js
@@ -3,25 +3,30 @@ import FIXTURES from '../fixtures';
 const verifier = require('../../dist/verifier');
 
 describe('verifier build test suite', function () {
-  it('works as expected with a v1 certificate', async function () {
+  it('throws a deprecation error with a v1 certificate', async function () {
     const certificate = new verifier.Certificate(FIXTURES.TestnetV1Valid);
-    await certificate.init();
-    const result = await certificate.verify();
-    expect(result.status).toBe(VERIFICATION_STATUSES.FAILURE);
-    expect(result.message).toBe('The address used to issue this Blockcerts does not belong to the claimed issuer.');
+    expect(async () => {
+      await certificate.init();
+    }).rejects.toThrow('Verification of v1 certificates is not supported by this component. ' +
+      'See the python cert-verifier for v1.1 verification ' +
+      'or the npm package cert-verifier-js-v1-legacy for v1.2 ' +
+      '(https://www.npmjs.com/package/@blockcerts/cert-verifier-js-v1-legacy)');
   });
 
   it('works as expected with a v2 certificate', async function () {
     const certificate = new verifier.Certificate(FIXTURES.MainnetV2Valid);
     await certificate.init();
     const result = await certificate.verify();
-    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
+    if (result.status === VERIFICATION_STATUSES.FAILURE) {
+      console.log(result.message);
+    }
     expect(result.message).toEqual({
       label: 'Verified',
       // eslint-disable-next-line no-template-curly-in-string
       description: 'This is a valid ${chain} certificate.',
       linkText: 'View transaction link'
     });
+    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
   });
 
   it('works as expected with a v3 certificate', async function () {
@@ -31,12 +36,12 @@ describe('verifier build test suite', function () {
     if (result.status === VERIFICATION_STATUSES.FAILURE) {
       console.log(result.message);
     }
-    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
     expect(result.message).toEqual({
       label: 'Verified',
       // eslint-disable-next-line no-template-curly-in-string
       description: 'This is a valid ${chain} certificate.',
       linkText: 'View transaction link'
     });
+    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
   });
 });

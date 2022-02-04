c2ea6dfd1366fdd2da110f957583798522a0b0dd
Julien Fraichot <julien.fraichot@hyland.com>
2022-02-04 13:19:54 +0100

    feat(BlockcertsV3): define VC as base model

--- src/models/BlockcertsV3.ts
+++ src/models/BlockcertsV3.ts
@@ -1,12 +1,51 @@
 import { Issuer } from './Issuer';
 import { MerkleProof2019 } from './MerkleProof2019';
 
-export interface BlockcertsV3 {
+export interface VerifiableCredential {
   '@context': string[];
   id: string;
   type: string[];
-  issuanceDate: string;
+  credentialStatus?: {
+    id: string;
+    type: string;
+    statusListIndex?: string;
+    statusListCredential?: string;
+  };
+  expirationDate?: string;
+  evidence?: Array<{
+    type: string[];
+    id?: string;
+    // more properties as defined per implementation
+  }>;
+  holder?: {
+    type?: string;
+    id?: string;
+  };
+  issued?: string;
+  issuanceDate?: string;
+  refreshService?: {
+    type?: string;
+    id?: string; // must be URI
+  };
+  termsOfUse?: Array<{
+    type: string;
+    id?: string;
+    // more properties as defined per implementation
+  }>;
+  validFrom?: string; // expect dateTime
+  validUntil?: string; // expect dateTime
+  proof: {
+    type: string;
+    created?: string;
+    proofValue: string;
+    proofPurpose?: string;
+    verificationMethod?: string;
+  };
+}
+
+export interface BlockcertsV3 extends VerifiableCredential{
   issuer: string | Issuer;
+  issuanceDate: string;
   credentialSubject: {
     id: string;
     publicKey?: string;
@@ -33,9 +72,4 @@ export interface BlockcertsV3 {
    * @deprecated v3 alpha only
    */
   metadataJson?: string;
-
-  /**
-   * @deprecated v3 alpha only
-   */
-  expirationDate?: string;
 }

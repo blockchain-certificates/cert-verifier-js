15774370b0cc0061a1eb8a0eadeaa8f4b698d841
Julien Fraichot <julien.fraichot@hyland.com>
2022-05-17 15:38:41 -0500

    feat(DID): expand issuer profile from did:key

--- test/e2e/verifier/proof-chain-example.test.ts
+++ test/e2e/verifier/proof-chain-example.test.ts
@@ -4,50 +4,14 @@ import sinon from 'sinon';
 import * as ExplorerLookup from '@blockcerts/explorer-lookup';
 import { universalResolverUrl } from '../../../src/domain/did/valueObjects/didResolver';
 import multipleProofsVerificationSteps from '../../assertions/verification-steps-v3-multiple-proofs';
+import didKeyDocument from '../../fixtures/did/did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs';
 
 describe('proof chain example', function () {
   beforeEach(function () {
     const requestStub = sinon.stub(ExplorerLookup, 'request');
     requestStub.withArgs({
       url: `${universalResolverUrl}/${fixture.issuer}`
-    }).resolves(JSON.stringify({
-      didDocument: {
-        '@context': [
-          'https://www.w3.org/ns/did/v1',
-          'https://w3id.org/security/suites/ed25519-2020/v1',
-          'https://w3id.org/security/suites/x25519-2020/v1'
-        ],
-        id: 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
-        verificationMethod: [
-          {
-            id: 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
-            type: 'Ed25519VerificationKey2020',
-            controller: 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
-            publicKeyMultibase: 'z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs'
-          }
-        ],
-        authentication: [
-          'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs'
-        ],
-        assertionMethod: [
-          'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs'
-        ],
-        capabilityDelegation: [
-          'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs'
-        ],
-        capabilityInvocation: [
-          'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs'
-        ],
-        keyAgreement: [
-          {
-            id: 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6LSryEyvxgv9paaRHFeJ3vbHvPhtETwh339P52eDT21xdb1',
-            type: 'X25519KeyAgreementKey2020',
-            controller: 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
-            publicKeyMultibase: 'z6LSryEyvxgv9paaRHFeJ3vbHvPhtETwh339P52eDT21xdb1'
-          }
-        ]
-      }
-    }));
+    }).resolves(JSON.stringify({ didDocument: didKeyDocument }));
   });
 
   afterEach(function () {

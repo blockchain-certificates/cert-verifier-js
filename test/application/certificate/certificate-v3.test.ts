ff945bd4f532afcc2d68f7c162a7e0597da2a0d5
Julien Fraichot <julien.fraichot@hyland.com>
2022-05-18 11:30:58 -0500

    test(V3): disconnect from internet

--- test/application/certificate/certificate-v3.test.ts
+++ test/application/certificate/certificate-v3.test.ts
@@ -1,13 +1,32 @@
 import { BLOCKCHAINS, Certificate } from '../../../src';
 import FIXTURES from '../../fixtures';
 import signatureAssertion from '../../assertions/testnet-v3.0-did-signature-merkle2019.json';
-import issuerProfileAssertion from '../../assertions/v3.0-issuer-profile.json';
 import didDocument from '../../fixtures/did/did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ.json';
+import sinon from 'sinon';
+import * as ExplorerLookup from '@blockcerts/explorer-lookup';
+import { universalResolverUrl } from '../../../src/domain/did/valueObjects/didResolver';
+import fixtureIssuerProfile from '../../fixtures/issuer-profile.json';
+import notAnIssuerProfile from '../../fixtures/v3/testnet-v3--no-did.json';
 
 const assertionTransactionId = '140ee9382a5c84433b9c89a5d9fea26c47415838b5841deb0c36a8a4b9121f2e';
 
 describe('Certificate entity test suite', function () {
   const fixture = FIXTURES.BlockcertsV3;
+  let requestStub;
+
+  beforeEach(function () {
+    requestStub = sinon.stub(ExplorerLookup, 'request');
+    requestStub.withArgs({
+      url: `${universalResolverUrl}/did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ`
+    }).resolves(JSON.stringify({ didDocument }));
+    requestStub.withArgs({
+      url: 'https://www.blockcerts.org/samples/3.0/issuer-blockcerts.json'
+    }).resolves(JSON.stringify(fixtureIssuerProfile));
+  });
+
+  afterEach(function () {
+    sinon.restore();
+  });
 
   describe('constructor method', function () {
     describe('given it is called with valid v3 certificate data', function () {
@@ -44,7 +63,7 @@ describe('Certificate entity test suite', function () {
       });
 
       it('should set the issuer property', function () {
-        const expectedOutput = JSON.parse(JSON.stringify(issuerProfileAssertion));
+        const expectedOutput = JSON.parse(JSON.stringify(fixtureIssuerProfile));
         expectedOutput.didDocument = didDocument;
         expect(certificate.issuer).toEqual(expectedOutput);
       });
@@ -123,9 +142,10 @@ describe('Certificate entity test suite', function () {
 
       describe('when the issuer profile URL yields a server error', function () {
         it('should throw an error', async function () {
-          const failingFixture = JSON.parse(JSON.stringify(fixture));
-          failingFixture.issuer += 'willfailfortests';
-          const certificate = new Certificate(failingFixture);
+          requestStub.withArgs({
+            url: 'https://www.blockcerts.org/samples/3.0/issuer-blockcerts.json'
+          }).rejects();
+          const certificate = new Certificate(fixture);
           await expect(certificate.init())
             .rejects
             .toThrow('Unable to get issuer profile');
@@ -134,9 +154,10 @@ describe('Certificate entity test suite', function () {
 
       describe('when the issuer profile URL is not of a issuer profile', function () {
         it('should throw an error', async function () {
-          const failingFixture = JSON.parse(JSON.stringify(fixture));
-          failingFixture.issuer = 'https://raw.githubusercontent.com/blockchain-certificates/cert-schema/master/cert_schema/3.0-alpha/context.json';
-          const certificate = new Certificate(failingFixture);
+          requestStub.withArgs({
+            url: 'https://www.blockcerts.org/samples/3.0/issuer-blockcerts.json'
+          }).resolves(JSON.stringify(notAnIssuerProfile));
+          const certificate = new Certificate(fixture);
           await expect(certificate.init())
             .rejects
             .toThrow('Unable to get issuer profile - retrieved file does not seem to be a valid profile');

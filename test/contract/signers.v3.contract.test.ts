eab03774830579cf041c2f8c9781b6d5e1e9b903
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-02 14:57:17 -0500

    test(MultiSign): add v2 signers contract test suite

--- test/contract/signers.v3.contract.test.ts
+++ test/contract/signers.v3.contract.test.ts
@@ -12,11 +12,9 @@ describe('Certificate API Contract test suite', function () {
   describe('signers property', function () {
     describe('given there is only one signature to the V3 document', function () {
       let instance;
-      let requestStub;
-      let lookForTxStub;
 
       beforeEach(async function () {
-        requestStub = sinon.stub(ExplorerLookup, 'request');
+        const requestStub = sinon.stub(ExplorerLookup, 'request');
         requestStub.withArgs({
           url: `${universalResolverUrl}/did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ`
         }).resolves(JSON.stringify({ didDocument }));
@@ -26,7 +24,7 @@ describe('Certificate API Contract test suite', function () {
         requestStub.withArgs({
           url: 'https://www.blockcerts.org/samples/3.0/revocation-list-blockcerts.json'
         }).resolves(JSON.stringify(v3RevocationList));
-        lookForTxStub = sinon.stub(domain.verifier, 'lookForTx').resolves({
+        sinon.stub(domain.verifier, 'lookForTx').resolves({
           remoteHash: '68df661ae14f926878aabbe5ca33e46376e8bfb397c1364c2f1fa653ecd8b4b6',
           issuingAddress: 'mgdWjvq4RYAAP5goUNagTRMx7Xw534S5am',
           time: '2022-04-05T18:45:30.000Z',

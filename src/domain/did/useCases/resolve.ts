f837c6aa686559d9620512468a6ea66ff814d08f
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-03 14:57:18 -0500

    chore(Did): remove network bypass set for manual test in BV

--- src/domain/did/useCases/resolve.ts
+++ src/domain/did/useCases/resolve.ts
@@ -3,7 +3,6 @@ import { request } from '@blockcerts/explorer-lookup';
 import DidResolver from '../valueObjects/didResolver';
 import { isDidKey } from '../../verifier/useCases/getIssuerProfile';
 import resolveDidKeyDocument from './resolveDidKeyDocument';
-import ionDidDocument from '../../../../test/fixtures/did/did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ.json';
 
 interface IUniversalResolverResponse {
   didResolutionMetadata?: any;
@@ -22,10 +21,6 @@ export default async function resolve (didUri: string, didResolverUrl = DidResol
   if (isDidKey(didUri)) {
     const didDocument = await resolveDidKeyDocument(didUri);
     return didDocument;
-  }
-
-  if (didUri === 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ') {
-    return ionDidDocument;
   }
   const universalResolverResponse: string = await request({ url: `${didResolverUrl}/${didUri}` });
   const parsedUniversalResolverResponse: IUniversalResolverResponse = JSON.parse(universalResolverResponse);

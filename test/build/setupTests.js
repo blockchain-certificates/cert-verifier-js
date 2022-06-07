0e3d0dea7ff4933dedbb23c27aefde73397eee9f
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-07 10:45:22 -0500

    chore(Test): fix build test

--- test/build/setupTests.js
+++ test/build/setupTests.js
@@ -3,6 +3,7 @@ import crypto from 'crypto';
 Object.defineProperty(global.self, 'crypto', {
   value: {
     // use node 15.x
-    subtle: crypto.webcrypto.subtle
+    subtle: crypto.webcrypto.subtle,
+    getRandomValues: () => new Uint32Array(10)
   }
 });

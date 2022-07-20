e27baec6bf4fbfd50458795aa55b97363eb38192
Julien Fraichot <julien.fraichot@hyland.com>
2022-07-20 13:23:45 -0500

    refactor(DidKey): support explicit did key suites (prevent rollup error with bbs suite)

--- src/domain/did/useCases/resolveDidKeyDocument.ts
+++ src/domain/did/useCases/resolveDidKeyDocument.ts
@@ -1,7 +1,18 @@
 import type { IDidDocument } from '../../../models/DidDocument';
-import * as didKey from '@transmute/did-key.js';
+import * as ed25519 from '@transmute/did-key-ed25519';
+import * as secp256k1 from '@transmute/did-key-secp256k1';
+
+const supportedSuiteMap = {
+  'did:key:z6Mk': ed25519,
+  'did:key:zQ3s': secp256k1
+};
 
 export default async function resolveDidKeyDocument (didKeyUri: string): Promise<IDidDocument> {
-  const { didDocument } = await didKey.resolve(didKeyUri, { accept: 'application/did+json' });
-  return didDocument;
+  const keyPrefix = didKeyUri.substring(0, 12);
+  if (supportedSuiteMap[keyPrefix]) {
+    const { didDocument } = await supportedSuiteMap[keyPrefix].resolve(didKeyUri, { accept: 'application/did+json' });
+    return didDocument;
+  }
+
+  throw new Error('Unsupported did:key suite');
 }

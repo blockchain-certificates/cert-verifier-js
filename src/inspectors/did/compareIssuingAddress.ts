5b0b0364e7d42151f534d851e7b79a63a5d52466
Julien Fraichot <julien.fraichot@hyland.com>
2022-11-21 15:23:44 +0100

    refactor(DID): remove unused code (moved to jsonld-signatures-merkleproof2019)

--- src/inspectors/did/compareIssuingAddress.ts
+++
@@ -1,9 +0,0 @@
-import { VerifierError } from '../../models';
-import domain from '../../domain';
-import { baseError } from './index';
-
-export default function compareIssuingAddress (issuingAddress: string, derivedIssuingAddress: string): void {
-  if (issuingAddress !== derivedIssuingAddress) {
-    throw new VerifierError('compareIssuingAddress', `${baseError} - ${domain.i18n.getText('errors', 'compareIssuingAddress')}`);
-  }
-}
--- src/inspectors/index.ts
+++ src/inspectors/index.ts
@@ -1,5 +1,4 @@
 import computeLocalHash from './computeLocalHash';
-import confirmDidSignature from './confirmDidSignature';
 import ensureHashesEqual from './ensureHashesEqual';
 import ensureMerkleRootEqual from './ensureMerkleRootEqual';
 import ensureNotExpired from './ensureNotExpired';
@@ -7,17 +6,12 @@ import ensureNotRevoked from './ensureNotRevoked';
 import ensureValidIssuingKey from './ensureValidIssuingKey';
 import ensureValidReceipt from './ensureValidReceipt';
 import isTransactionIdValid from './isTransactionIdValid';
-import compareIssuingAddress from './did/compareIssuingAddress';
 import controlVerificationMethod from './did/controlVerificationMethod';
-import deriveIssuingAddressFromPublicKey from './did/deriveIssuingAddressFromPublicKey';
 import retrieveVerificationMethodPublicKey from './did/retrieveVerificationMethodPublicKey';
 
 export {
-  compareIssuingAddress,
   computeLocalHash,
-  confirmDidSignature,
   controlVerificationMethod,
-  deriveIssuingAddressFromPublicKey,
   ensureHashesEqual,
   ensureMerkleRootEqual,
   ensureNotExpired,

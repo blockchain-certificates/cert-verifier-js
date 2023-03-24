99ec982347fd7c7345292ef7efb9a4a7ce6283e2
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-24 15:44:46 +0100

    chore(Jest): add comment

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   ],
   transform: {
     '^.+\\.(ts)$': 'ts-jest',
-    '^.+\\.(js)$': 'babel-jest'
+    '^.+\\.(js)$': 'babel-jest' // needed for node_module deps
   },
   transformIgnorePatterns: [
     'node_modules/(?!(@digitalbazaar|@blockcerts|base58-universal|base64url-universal|crypto-ld)/)'

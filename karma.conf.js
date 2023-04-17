8d2d8ab20512cd6c173c65f322a4f972c55e2392
Julien Fraichot <julien.fraichot@hyland.com>
2023-04-17 19:12:38 -0300

    test(Build): bypass internet connection for iife build test

--- karma.conf.js
+++ karma.conf.js
@@ -13,6 +13,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
+      'test/build/mocks/FakeXmlHttpRequest.iife.js',
       'dist/verifier-iife.js',
       'test/build/verifier-iife.test.karma.build.js'
     ],

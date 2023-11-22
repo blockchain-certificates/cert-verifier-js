482058a58cfc8b4681a88300b7e7c48d7b034985
Julien Fraichot <julien.fraichot@hyland.com>
2023-11-22 17:24:46 +0100

    fix(v1): manage normalized document similar to jsonld@1.8.2 (current cvjs v1 state)

--- src/inspectors/computeLocalHash.ts
+++ src/inspectors/computeLocalHash.ts
@@ -7,9 +7,6 @@ import { getText } from '../domain/i18n/useCases';
 import type { Blockcerts, UnsignedBlockcerts } from '../models/Blockcerts';
 import retrieveUnsignedBlockcerts from '../parsers/helpers/retrieveUnsignedBlockcerts';
 import { isObject } from '../helpers/object';
-// import { retrieveBlockcertsVersion } from '../parsers';
-// import type Versions from '../constants/certificateVersions';
-// import { isV1 } from '../constants/certificateVersions';
 
 export function getUnmappedFields (normalized: string): string[] | null {
   const normalizedArray = normalized.split('\n');
@@ -24,22 +21,37 @@ export function getUnmappedFields (normalized: string): string[] | null {
   return null;
 }
 
+function adjustV1ContextForLegacySupport (contextDocument: any): any {
+  if (contextDocument['@context'][0]['image:signature'] == null) {
+    return contextDocument;
+  }
+  const ctxDocument = JSON.parse(JSON.stringify(contextDocument));
+  // prevent term in form of IRI must expand to definition error
+  // see https://github.com/digitalbazaar/jsonld.js/issues/542
+  ctxDocument['@context'][0].__IMAGE__SIGNATURE__ = contextDocument['@context'][0]['image:signature'];
+  delete ctxDocument['@context'][0]['image:signature'];
+  return ctxDocument;
+}
+
 export default async function computeLocalHash (document: Blockcerts): Promise<string> {
   // the previous implementation was using a reference of @context, thus always adding @vocab to @context,
   // thus passing the information down to jsonld regardless of the configuration option. We explicitly do that now,
   // since we want to make sure unmapped fields are detected.
-  // const isV1Document = isV1(retrieveBlockcertsVersion(document['@context']) as Versions);
-  // console.log('isV1', isV1Document);
   if (Array.isArray(document['@context']) && !document['@context'].find((context: any) => isObject(context) && '@vocab' in context)) {
     document['@context'].push({ '@vocab': 'http://fallback.org/' });
   }
   const theDocument: UnsignedBlockcerts = retrieveUnsignedBlockcerts(document);
 
+  if ((theDocument as any).document?.assertion?.['image:signature'] != null) {
+    (theDocument as any).document.assertion.__IMAGE__SIGNATURE__ = (theDocument as any).document.assertion['image:signature'];
+    delete (theDocument as any).document.assertion['image:signature'];
+  }
+
   const customLoader = function (url): any {
     if (url in preloadedContexts) {
       return {
         contextUrl: null,
-        document: preloadedContexts[url],
+        document: adjustV1ContextForLegacySupport(preloadedContexts[url]),
         documentUrl: url
       };
     }
@@ -49,7 +61,8 @@ export default async function computeLocalHash (document: Blockcerts): Promise<s
   const normalizeArgs: any = {
     algorithm: 'URDNA2015',
     format: 'application/nquads',
-    documentLoader: customLoader
+    documentLoader: customLoader,
+    safe: false
   };
 
   let normalizedDocument;
@@ -60,16 +73,13 @@ export default async function computeLocalHash (document: Blockcerts): Promise<s
     throw new VerifierError('computeLocalHash', getText('errors', 'failedJsonLdNormalization'));
   }
 
-  return '';
-
-  // const unmappedFields: string[] = getUnmappedFields(normalizedDocument);
-  // if (unmappedFields) {
-  //   throw new VerifierError(
-  //     'computeLocalHash',
-  //     `${getText('errors', 'foundUnmappedFields')}: ${unmappedFields.join(', ')}`
-  //   );
-  // } else {
-  //   console.log(sha256(toUTF8Data(normalizedDocument)));
-  //   return sha256(toUTF8Data(normalizedDocument));
-  // }
+  const unmappedFields: string[] = getUnmappedFields(normalizedDocument);
+  if (unmappedFields) {
+    throw new VerifierError(
+      'computeLocalHash',
+      `${getText('errors', 'foundUnmappedFields')}: ${unmappedFields.join(', ')}`
+    );
+  } else {
+    return sha256(toUTF8Data(normalizedDocument));
+  }
 }
--- test/application/inspectors/computeLocalHash.test.ts
+++ test/application/inspectors/computeLocalHash.test.ts
@@ -1,6 +1,5 @@
-// import blockcertsV3Fixture from '../../fixtures/v3/testnet-v3-did.json';
 import blockcertsV1Fixture from '../../fixtures/v1/testnet-valid-1.2.json';
-import computeLocalHash/*, { getUnmappedFields }*/ from '../../../src/inspectors/computeLocalHash';
+import computeLocalHash, { getUnmappedFields } from '../../../src/inspectors/computeLocalHash';
 
 describe('computeLocalHash test suite', function () {
   let fixture;
@@ -11,43 +10,44 @@ describe('computeLocalHash test suite', function () {
   describe('given it receives a document', function () {
     it('should return the SHA-256 hashed version', async function () {
       const output = await computeLocalHash(fixture);
-      expect(output).toBe('68f3ede17fdb67ffd4a5164b5687a71f9fbb68da803b803935720f2aa38f7728');
+      expect(output).toBe('8e0d1c50214c497d25988f4066f38d199f7b4329ed87ab52b726729076fff149');
     });
   });
 
-  // describe('given it receives the document has unmapped fields', function () {
-  //   it('should throw', async function () {
-  //     fixture.testUnmapped = 'this field is not mapped';
-  //     await expect(async () => {
-  //       await computeLocalHash(fixture);
-  //     }).rejects.toThrow('Found unmapped fields during JSON-LD normalization: testUnmapped');
-  //   });
-  // });
-  //
-  // describe('getUnmappedFields method', function () {
-  //   describe('given it finds no unmapped fields', function () {
-  //     it('should return null', function () {
-  //       const testString = '<did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .';
-  //       expect(getUnmappedFields(testString)).toEqual(null);
-  //     });
-  //   });
-  //
-  //   describe('given it finds some unmapped fields', function () {
-  //     it('should return a sorted list of the fields', function () {
-  //       const testString = `<did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .
-  //           <did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/surname> "Fraichot" .
-  //           <did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/email> "julien.fraichot@hyland.com" .`;
-  //       expect(getUnmappedFields(testString)).toEqual(['email', 'surname']);
-  //     });
-  //
-  //     describe('and some fields are duplicated', function () {
-  //       it('should return a list of deduped fields', function () {
-  //         const testString = `<did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/email> "julien.fraichot@hyland.com" .
-  //            <did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .
-  //            <https://auto-certificates.learningmachine.io/issuer/c6890ca6-ed91-de07-3b61-71702b9cbd3f.json> <http://fallback.org/email> "julien.fraichot@hyland.com" .`;
-  //         expect(getUnmappedFields(testString)).toEqual(['email']);
-  //       });
-  //     });
-  //   });
-  // });
+  xdescribe('given it receives the document has unmapped fields', function () {
+    // disabled as we disabled safe mode for legacy certs that may have unmapped fields
+    it('should throw', async function () {
+      fixture.testUnmapped = 'this field is not mapped';
+      await expect(async () => {
+        await computeLocalHash(fixture);
+      }).rejects.toThrow('Found unmapped fields during JSON-LD normalization: testUnmapped');
+    });
+  });
+
+  describe('getUnmappedFields method', function () {
+    describe('given it finds no unmapped fields', function () {
+      it('should return null', function () {
+        const testString = '<did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .';
+        expect(getUnmappedFields(testString)).toEqual(null);
+      });
+    });
+
+    describe('given it finds some unmapped fields', function () {
+      it('should return a sorted list of the fields', function () {
+        const testString = `<did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .
+            <did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/surname> "Fraichot" .
+            <did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/email> "julien.fraichot@hyland.com" .`;
+        expect(getUnmappedFields(testString)).toEqual(['email', 'surname']);
+      });
+
+      describe('and some fields are duplicated', function () {
+        it('should return a list of deduped fields', function () {
+          const testString = `<did:example:ebfeb1f712ebc6f1c276e12ec21> <http://fallback.org/email> "julien.fraichot@hyland.com" .
+             <did:example:ebfeb1f712ebc6f1c276e12ec21> <https://schema.org/name> "Julien Fraichot" .
+             <https://auto-certificates.learningmachine.io/issuer/c6890ca6-ed91-de07-3b61-71702b9cbd3f.json> <http://fallback.org/email> "julien.fraichot@hyland.com" .`;
+          expect(getUnmappedFields(testString)).toEqual(['email']);
+        });
+      });
+    });
+  });
 });

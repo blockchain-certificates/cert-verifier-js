253e9714eefb26b5be5765260e07623c6bc83d77
Julien Fraichot <julien.fraichot@hyland.com>
2024-06-13 16:03:59 +0200

    feat(SelectiveDisclosure): add ecdsa-sd-2023 verification support

--- src/suites/EcdsaSd2023.ts
+++ src/suites/EcdsaSd2023.ts
@@ -1,28 +1,29 @@
 import domain from '../domain';
 import jsigs from 'jsonld-signatures';
 import jsonld from 'jsonld';
-import * as ecdsaSd2023Cryptosuite from '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
+import { createVerifyCryptosuite } from '@digitalbazaar/ecdsa-sd-2023-cryptosuite';
+import { DataIntegrityProof } from '@digitalbazaar/data-integrity';
 import { Suite } from '../models/Suite';
 import { preloadedContexts } from '../constants';
-import { deepCopy } from '../helpers/object';
+import * as inspectors from '../inspectors';
 import type { Blockcerts } from '../models/Blockcerts';
 import type { Issuer } from '../models/Issuer';
 import type VerificationSubstep from '../domain/verifier/valueObjects/VerificationSubstep';
 import type { SuiteAPI } from '../models/Suite';
 import type { BlockcertsV3, VCProof } from '../models/BlockcertsV3';
 import type { IDidDocument } from '../models/DidDocument';
+import { VerifierError } from '../models';
 
 const { purposes: { AssertionProofPurpose } } = jsigs;
-// const { createVerifyCryptosuite } = ecdsaSd2023Cryptosuite;
 
 enum SUB_STEPS {
-  // retrieveVerificationMethodPublicKey = 'retrieveVerificationMethodPublicKey',
+  retrieveVerificationMethodPublicKey = 'retrieveVerificationMethodPublicKey',
   checkDocumentSignature = 'checkDocumentSignature'
 }
 
 export default class EcdsaSd2023 extends Suite {
   public verificationProcess = [
-    // SUB_STEPS.retrieveVerificationMethodPublicKey,
+    SUB_STEPS.retrieveVerificationMethodPublicKey,
     SUB_STEPS.checkDocumentSignature
   ];
 
@@ -32,6 +33,7 @@ export default class EcdsaSd2023 extends Suite {
   public type = 'EcdsaSd2023';
   public cryptosuite = 'ecdsa-sd-2023';
   public publicKey: string;
+  public verificationKey: any;
 
   constructor (props: SuiteAPI) {
     super(props);
@@ -42,7 +44,6 @@ export default class EcdsaSd2023 extends Suite {
     this.issuer = props.issuer;
     this.proof = props.proof as VCProof;
     this.validateProofType();
-    console.log('ecdsa sd', this.documentToVerify);
   }
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -118,7 +119,10 @@ export default class EcdsaSd2023 extends Suite {
     }
   }
 
-  private generateDocumentLoader (): any {
+  private generateDocumentLoader (documents: Array<{ url: string; value: string }> = []): any {
+    documents.forEach(document => {
+      preloadedContexts[document.url] = document.value;
+    });
     preloadedContexts[(this.documentToVerify as BlockcertsV3).issuer as string] = this.getTargetVerificationMethodContainer();
     const customLoader = function (url): any {
       if (url in preloadedContexts) {
@@ -133,41 +137,64 @@ export default class EcdsaSd2023 extends Suite {
     return customLoader;
   }
 
-  private retrieveInitialDocument (): BlockcertsV3 {
-    const document: BlockcertsV3 = deepCopy<BlockcertsV3>(this.documentToVerify as BlockcertsV3);
-    if (Array.isArray(document.proof)) {
-      const initialProof = document.proof.find(p => p.type === this.type);
-      delete document.proof;
-      document.proof = initialProof;
-    }
-    return document;
+  private getErrorMessage (verificationStatus): string {
+    return verificationStatus.results[0].error.cause.message;
   }
 
   private getTargetVerificationMethodContainer (): Issuer | IDidDocument {
     return this.issuer.didDocument ?? this.issuer;
   }
 
+  private async retrieveVerificationMethodPublicKey (): Promise<void> {
+    this.verificationKey = await this.executeStep(
+      SUB_STEPS.retrieveVerificationMethodPublicKey,
+      async (): Promise<any> => {
+        const verificationMethodPublicKey = inspectors.retrieveVerificationMethodPublicKey(
+          this.getTargetVerificationMethodContainer(),
+          this.proof.verificationMethod
+        );
+
+        if (!verificationMethodPublicKey) {
+          throw new VerifierError(SUB_STEPS.retrieveVerificationMethodPublicKey, 'Could not derive the verification key');
+        }
+
+        // TODO: revoked property should exist but we are currently using a forked implementation which does not expose it
+        if ((verificationMethodPublicKey as any).revoked) {
+          throw new VerifierError(SUB_STEPS.retrieveVerificationMethodPublicKey, 'The verification key has been revoked');
+        }
+
+        return verificationMethodPublicKey;
+      },
+      this.type
+    );
+  }
+
   private async checkDocumentSignature (): Promise<void> {
     await this.executeStep(
       SUB_STEPS.checkDocumentSignature,
       async (): Promise<void> => {
-        console.log('ecdsa sd checkDocumentSignature');
-        // const suite = new Secp256k1VerificationSuite({ key: this.verificationKey });
-        // // TODO: date property should exist but we are currently using a forked implementation which does not expose it
-        // (suite as any).date = new Date(Date.now()).toISOString();
-        //
-        // const verificationStatus = await jsigs.verify(this.retrieveInitialDocument(), {
-        //   suite,
-        //   purpose: new AssertionProofPurpose(),
-        //   documentLoader: this.generateDocumentLoader()
-        // });
-        //
-        // if (!verificationStatus.verified) {
-        //   console.error(JSON.stringify(verificationStatus, null, 2));
-        //   throw new VerifierError(SUB_STEPS.checkDocumentSignature, `The document's ${this.type} signature could not be confirmed`);
-        // } else {
-        //   console.log('Credential Secp256k1 signature successfully verified');
-        // }
+        const suite = new DataIntegrityProof({
+          cryptosuite: createVerifyCryptosuite({ algorithm: 'K-256' })
+        });
+        const verificationMethod = this.documentToVerify.proof.verificationMethod;
+        const verificationStatus = await jsigs.verify(this.documentToVerify, {
+          suite,
+          purpose: new AssertionProofPurpose(),
+          documentLoader: this.generateDocumentLoader([
+            {
+              url: verificationMethod,
+              value: this.verificationKey
+            }
+          ])
+        });
+
+        if (!verificationStatus.verified) {
+          console.error(JSON.stringify(verificationStatus, null, 2));
+          throw new VerifierError(SUB_STEPS.checkDocumentSignature,
+            `The document's ${this.type} signature could not be confirmed: ${this.getErrorMessage(verificationStatus)}`);
+        } else {
+          console.log(`Credential ${this.type} signature successfully verified`);
+        }
       },
       this.type
     );
--- test/e2e/verifier/ecdsa-sd-2023-signed.test.ts
+++ test/e2e/verifier/ecdsa-sd-2023-signed.test.ts
@@ -1,10 +1,12 @@
-import { describe, it, expect, vi } from 'vitest';
+import { describe, it, expect, vi, beforeAll, afterAll } from 'vitest';
 import { Certificate, VERIFICATION_STATUSES } from '../../../src';
-import fixture from '../../fixtures/v3/ecdsa-sd-2023-derived-credential.json';
+import fixture from '../../fixtures/v3/ecdsa-sd-2023-signed-credential.json';
 import fixtureIssuerProfile from '../../fixtures/issuer-blockcerts.json';
 
 describe('ecdsa-sd-2023 signed and derived document test suite', function () {
-  it('should verify successfully', async function () {
+  let result;
+
+  beforeAll(async function () {
     vi.mock('@blockcerts/explorer-lookup', async (importOriginal) => {
       const explorerLookup = await importOriginal();
       return {
@@ -18,8 +20,18 @@ describe('ecdsa-sd-2023 signed and derived document test suite', function () {
     });
     const certificate = new Certificate(fixture as any);
     await certificate.init();
-    const result = await certificate.verify();
+    result = await certificate.verify();
+  });
 
-    expect(result.status).toBe(VERIFICATION_STATUSES.SUCCESS);
+  afterAll(function () {
+    vi.restoreAllMocks();
+  });
+
+  it('should fail verification', function () {
+    expect(result.status).toBe(VERIFICATION_STATUSES.FAILURE);
+  });
+
+  it('should expose the expected error message', function () {
+    expect(result.message).toBe('The document\'s EcdsaSd2023 signature could not be confirmed: "proof.proofValue" must be a derived proof.');
   });
 });

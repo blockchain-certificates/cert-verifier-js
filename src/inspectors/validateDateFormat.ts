d74db96489b1d231876f5b2e353f0f4ddb710bc1
Julien Fraichot <julien.fraichot@hyland.com>
2024-10-15 16:10:11 +0200

    style(Types): fix mistyping

--- src/inspectors/validateDateFormat.ts
+++ src/inspectors/validateDateFormat.ts
@@ -4,7 +4,7 @@ import { SUB_STEPS } from '../domain/verifier/entities/verificationSteps';
 import { getText } from '../domain/i18n/useCases';
 import type { DatesToValidate } from '../domain/verifier/useCases/getDatesToValidate';
 
-export default function validateDateFormat (dates: DatesToValidate): void {
+export default function validateDateFormat (dates: DatesToValidate[]): void {
   for (const { dateTimeStamp, property } of dates) {
     if (!validateDateTimeStamp(dateTimeStamp)) {
       console.error('Date', dateTimeStamp, 'is not valid:', property);

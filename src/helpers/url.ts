49c3f6eae4c1dedc1a9a2446bff1568724f75ee9
Julien Fraichot <julien.fraichot@hyland.com>
2024-11-12 17:42:56 +0100

    feat(Verification): add VC format verification

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -2,3 +2,12 @@ export function safelyAppendUrlParameter (url: string, parameterKey: string, par
   const separator = url.includes('?') ? '&' : '?';
   return `${url}${separator}${parameterKey}=${parameterValue}`;
 }
+
+export function isValidUrl (url: string): boolean {
+  try {
+    const parsedUrl = new URL(url);
+    return !parsedUrl.pathname.includes(' ') && !parsedUrl.hostname.includes(' ') && url.includes(':');
+  } catch {
+    return false;
+  }
+}

882791cba02225cbe2810317174a9727efe5d8f2
Julien Fraichot <julien.fraichot@hyland.com>
2026-03-10 16:12:09 +0100

    test(DID): add test

--- test/application/domain/did/useCases/resolveDidKeyDocument.test.ts
+++ test/application/domain/did/useCases/resolveDidKeyDocument.test.ts
@@ -1,8 +1,16 @@
-import {describe, it} from "vitest";
-import resolveDidKeyDocument from "../../../../../src/domain/did/useCases/resolveDidKeyDocument";
+import {describe, expect, it} from 'vitest';
+import resolveDidKeyDocument from '../../../../../src/domain/did/useCases/resolveDidKeyDocument';
+import didDocumentSecp256k1 from '../../../../assertions/did:key:zQ3shWbMURApEGeYEYbyfMoPbuW5qCmM7gtWkwnhGf2LLkv2M.json';
+import didDocumentEd25519 from '../../../../fixtures/did/did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs';
 
 describe('resolveDidKeyDocument DID domain useCase test suite', function () {
-  it('resolves a DID document from a secp256k1 key', function () {
-    // resolveDidKeyDocument
+  it('resolves a DID document from a secp256k1 key', async function () {
+    const result = await resolveDidKeyDocument('did:key:zQ3shWbMURApEGeYEYbyfMoPbuW5qCmM7gtWkwnhGf2LLkv2M');
+    expect(result).toEqual(didDocumentSecp256k1);
+  });
+
+  it('resolves a DID document from a ed25519 key', async function () {
+    const result = await resolveDidKeyDocument('did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs');
+    expect(result).toEqual(didDocumentEd25519);
   });
 });

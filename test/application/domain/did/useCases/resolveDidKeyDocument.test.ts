a8e9f6c763899c433ac3663c6d32ee7401c78292
Julien Fraichot <julien.fraichot@hyland.com>
2026-03-10 16:04:04 +0100

    fix(Deps): remove keyto and secp256k1 direct dependency

---
+++ test/application/domain/did/useCases/resolveDidKeyDocument.test.ts
@@ -0,0 +1,8 @@
+import {describe, it} from "vitest";
+import resolveDidKeyDocument from "../../../../../src/domain/did/useCases/resolveDidKeyDocument";
+
+describe('resolveDidKeyDocument DID domain useCase test suite', function () {
+  it('resolves a DID document from a secp256k1 key', function () {
+    // resolveDidKeyDocument
+  });
+});

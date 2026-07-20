9fa3d1592b971b4e2e05ecc6b52e65c54b6d6f11
Julien Fraichot <julien.fraichot@hyland.com>
2026-07-20 11:55:49 +0100

    chore(Tests): do not run coverage on test or test:watch

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     coverage: {
-      enabled: true,
       provider: 'istanbul', // or 'v8'
       reporter: ['text', 'lcov'],
       reportsDirectory: './coverage'
